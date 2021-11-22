462ab8a561aad95b4af3d3a64c41a56bc93ae6f1
PedroFilho <psfilho81@hotmail.com>
2021-11-22 20:16:23 -0300

    Create Aritimetica.java

---
+++ CienciaDaComputacao/Capitulo01/Exemplos1_2/Aritimetica.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+
+// cria a classe Aritimética
+public class Aritimetica {
+    // função principal
+    public static void main(String[] args) {
+
+        // entrada de dados
+        int a = 5; // primeiro valor
+        int b = 2; // segundo valor
+
+        // calcula
+        int sum = a + b;
+        int prod = a * b;
+        int quot = a / b;
+        int rem = a % b;
+
+        // mostra resultado
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println(a + " * " + b + " = " + prod);
+        System.out.println(a + " / " + b + " = " + quot);
+        System.out.println(a + " % " + b + " = " + rem);
+        System.out.println(a + " = " + quot + " * " + b + " + " + rem);
+    } // fim main
+} // fim class
