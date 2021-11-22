9979ff993061c02f167b609fb7f603bcfa0bc718
PedroFilho <psfilho81@hotmail.com>
2021-11-22 19:14:21 -0300

    Ruler

---
+++ CienciaDaComputacao/Capitulo01/Exemplos1_2/Ruler.java
@@ -0,0 +1,18 @@
+public class Ruler { 
+    public static void main(String[] args) 
+    {
+        // variáveis
+        String ruler1 = " 1 ";
+        String ruler2 = ruler1 + "2" + ruler1;
+        String ruler3 = ruler2 + "3" + ruler2;
+        String ruler4 = ruler3 + "4" + ruler3;
+        String ruler5 = ruler4 + "5" + ruler4;
+
+        // imprime resultado
+        System.out.println(ruler1);
+        System.out.println(ruler2);
+        System.out.println(ruler3);
+        System.out.println(ruler4);
+        System.out.println(ruler5);
+    } // fim main
+} // fim class
