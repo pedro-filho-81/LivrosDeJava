c9ea5c932e25bfc3a47a420d360fb1fe857d6b8b
PedroFilho <psfilho81@hotmail.com>
2021-11-22 15:15:29 -0300

    Olá Mundo
    Primeiro programa

---
+++ CienciaDaComputacao/Capitulo01/OlaMundo.java
@@ -0,0 +1,13 @@
+// barra barra => serva para comentários de linha
+class OlaMundo // cria a classe OlaMundo
+{ // abre o bloco da classe
+
+    // cria a função principal pública main( parâmetro )
+    public static void main(String[] args )
+    { // abre o bloco da função main
+
+        // comando para imprimir uma string na tela do computador
+        System.out.println("Olá, Mundo Java!"); // string => texto entre aspas duplas
+        
+    } // fim da função principal main
+} // fim da classe OlaMundo
