786d29842c6c16816fd63ccd9dc8a8779d9884b5
PedroFilho <psfilho81@hotmail.com>
2021-11-23 08:51:18 -0300

    Inteiro Aleatório

---
+++ CienciaDaComputacao/Capitulo01/Exemplos1_2/InteiroAleatorio.java
@@ -0,0 +1,17 @@
+
+public class InteiroAleatorio {
+    public static void main(String[] args) {
+
+        // variável recebe valor do usuário
+        int n = 5;
+
+        // gera número aleatório entre 0.0 e 1.0
+        double r = Math.random();
+
+        // // variável recebe o valor do produto entre r e n
+        int value = (int) (r * n);
+
+        // mostra o resultado
+        System.out.println(value);
+    } // fim main
+} // fim classe
