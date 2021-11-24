c62db9351f36768cb7361112b1ebe1dc4cbb2306
PedroFilho <psfilho81@hotmail.com>
2021-11-24 15:57:48 -0300

    Cara ou Coroa

---
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/CaraOuCoroa01.java
@@ -0,0 +1,16 @@
+// jogo da moeda cara ou coroa
+public class CaraOuCoroa01 {
+
+    public static void main(String[] args) {
+
+        // Math.random() < 0,5 =>
+        // retorna um valor entre 0 e 1
+        if (Math.random() < 0.5) // se for zero
+            // imprime cara
+            System.out.println("Cara");
+        else // se não
+             // imprime coroa
+            System.out.println("Coroa");
+
+    } // fim main
+} // fim classe
