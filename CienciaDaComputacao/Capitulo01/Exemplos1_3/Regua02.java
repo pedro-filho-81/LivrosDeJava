9c52fc988b6704dd5c138483121bb13d809e0658
PedroFilho <psfilho81@hotmail.com>
2021-11-24 16:07:54 -0300

    Régua

---
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/Regua02.java
@@ -0,0 +1,21 @@
+package Exemplos1_3;
+
+// Imprime os comprimentos relativos das subdivisões em uma régua.
+public class Regua02 {
+    public static void main(String[] args) {
+
+        // variáveis
+        String ruler1 = " 1 ";
+        String ruler2 = ruler1 + "2" + ruler1;
+        String ruler3 = ruler2 + "3" + ruler2;
+        String ruler4 = ruler3 + "4" + ruler3;
+        String ruler5 = ruler4 + "5" + ruler4;
+
+        // mostra o resultado
+        System.out.println(ruler1);
+        System.out.println(ruler2);
+        System.out.println(ruler3);
+        System.out.println(ruler4);
+        System.out.println(ruler5);
+    } // fim main
+} // fim classe
