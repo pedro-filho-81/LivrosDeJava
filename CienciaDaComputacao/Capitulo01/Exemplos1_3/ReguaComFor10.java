c987ded488f66b7ee46f7cc5ae03174e2d601b89
PedroFilho <psfilho81@hotmail.com>
2021-11-25 10:44:55 -0300

    Régua com For

---
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/ReguaComFor10.java
@@ -0,0 +1,29 @@
+package Exemplos1_3;
+
+import java.util.Scanner;
+
+public class ReguaComFor10 {
+
+    public static void main(String[] args) {
+
+        // cria o objeto input e inicializa
+        Scanner input = new Scanner(System.in);
+
+        // entrada de dados
+        System.out.print("Digite o tamanho da regua: ");
+        int n = input.nextInt();
+
+        // regua comaça com 0
+        String ruler = " ";
+
+        // repete n vezes
+        for (int i = 1; i <= n; i++) {
+
+            // concatenate a ruler of order 0, the number i, and a ruler of order 0
+            ruler = ruler + i + ruler;
+
+            // imprime n vezes
+            System.out.println(ruler);
+        } // fim for
+    } // fim main
+} // fim classe
