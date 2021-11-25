330e56add51e5fffbf6f038413d99bfb6be0b6ef
PedroFilho <psfilho81@hotmail.com>
2021-11-25 17:54:31 -0300

    Atualização

--- CienciaDaComputacao/Capitulo01/Exemplos1_3/DecimalParaBinario13.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/DecimalParaBinario13.java
@@ -11,31 +11,42 @@ public class DecimalParaBinario13 {
 
         // entrada de dados
         System.out.print("Digite um inteiro: ");
-        // read in the command-line argument
+        // lê um argumento da linha de comando
         int n = input.nextInt();
 
         // defina a potência para a maior potência de 2 que é <= n
-        int power = 1;
-        while (power <= n / 2) {
+        int power = 1; // power começa com 1
+
+        // enquanto qower menor ou igual a metade de n
+        while (power <= n / 2) { // faça
+
+            // calcula a potência de de 2 até power ser maior que n
             power *= 2;
+            System.out.println("Power1 = " + power);
         } // fim while
 
         // verifica a presença de potências de 2 em n, da maior para a menor
+        // enquanto power maior que zero faça
         while (power > 0) {
-
+           
             // potência não está presente em n
+            // se n menor que power
             if (n < power) {
-                System.out.print(0);
+                // imprima zero 0
+                System.out.print("  n = " + n + " < power = " + power + "  " + 0);
             } // fim if
 
             // a potência está presente em n, então subtraia a potência de n
-            else {
-                System.out.print(1);
+            else { // se n maior que power 
+                // imprima 1
+                System.out.print("  n = " + n + " > power = " + power + "  " + 1);
+                // diminua de n o valor de power
                 n -= power;
             } // fim else
 
             // próxima menor potência de 2
-            power /= 2;
+            power /= 2; // divida o valor de power por 2
+            System.out.println();
 
         } // fim while
 
