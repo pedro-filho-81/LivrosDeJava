5e59b1799e65c0d2a2002c24fd900cd0cf24a906
PedroFilho <psfilho81@hotmail.com>
2021-11-25 11:02:39 -0300

    Raiz Quadrada

---
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/RaizQuadrada12.java
@@ -0,0 +1,29 @@
+package Exemplos1_3;
+
+import java.util.Scanner;
+
+public class RaizQuadrada12 {
+
+    public static void main(String[] args) {
+
+        // cria o objeto input e inicializa
+        Scanner input = new Scanner(System.in);
+
+        // Entrada de dados
+        System.out.print("Digite um número: ");
+        double c = input.nextInt();
+
+        // calcula
+        double epsilon = 1.0e-15; // relative error tolerance
+        double t = c; // estimate of the square root of c
+
+        // repeatedly apply Newton update step until desired precision is achieved
+        while (Math.abs(t - c / t) > epsilon * t) {
+            t = (c / t + t) / 2.0;
+        } // fim while
+
+        // print out the estimate of the square root of c
+        System.out.println(t);
+
+    } // fim main
+} // fim classe
