6df64ecce786b162e6d903d3cc0e730b1a47d598
PedroFilho <psfilho81@hotmail.com>
2021-11-27 20:32:57 -0300

    Tabela boolean OU EXCLUSIVO

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_2/OuExclusivoBoolean05.java
@@ -0,0 +1,23 @@
+public class OuExclusivoBoolean05 {
+    public static void main(String[] args) {
+        
+        System.out.println("\nTABELA VERDADE OU EXCLUSIVO BOOLEAN");
+
+        // variáveis
+        boolean p = true;
+        boolean q = true;
+        System.out.println("p = " + p + " q = " + q + " p # q = " + (p ^ q) );
+
+        p = true;
+        q = false;
+        System.out.println("p = " + p + " q = " + q + " p # q = " + (p ^ q) );
+
+        p = false;
+        q = true;
+        System.out.println("p = " + p + " q = " + q + " p # q = " + (p ^ q) );
+
+        p = false;
+        q = false;
+        System.out.println("p = " + p + " q = " + q + " p # q = " + (p ^ q) );
+    } // FIM MAIN    
+} // fim classe
