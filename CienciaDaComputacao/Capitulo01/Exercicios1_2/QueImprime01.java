7749ec829b3b2f7d574aaa54f37494d5720e09ca
PedroFilho <psfilho81@hotmail.com>
2021-11-27 16:42:43 -0300

    Passando Valor

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_2/QueImprime01.java
@@ -0,0 +1,25 @@
+public class QueImprime01 {
+
+    /*
+        1.2.1 Suponha que a e b são variáveis inteiras. O que a seguinte 
+        sequência de afirmações faz? int t = a; b = t; a = b;
+        Sedgewick, Robert; Wayne, Kevin. Ciência da computação (p. 44). 
+        Pearson Education. Edição do Kindle.
+        Pedro, 27/11/2021
+    */
+
+    public static void main(String[] args) { 
+
+        // variáveis
+        int a = 1; // a recebe 1
+        int b = 2; // b recebe 2
+        int t = a; // t recebe o valor de a = 1
+            b = t; // b recebe o valor de t = 1
+            a = b; // a recebe o valor de b = 1
+
+            // a, b e t estão com o valor 1
+        
+        // Todos imprimem o número 1
+        System.out.println("a = " + a + " b = " + b + "t = " + t);
+    }// fim main
+} // fim classe
