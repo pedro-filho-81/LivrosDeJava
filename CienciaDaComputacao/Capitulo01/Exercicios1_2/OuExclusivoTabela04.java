48e26b178908ff81575558e0a62d50691512efb5
PedroFilho <psfilho81@hotmail.com>
2021-11-27 19:45:01 -0300

    Tabela Verdade OU EXCLUSIVO

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_2/OuExclusivoTabela04.java
@@ -0,0 +1,13 @@
+class OuExclusivo04 {
+    public static void main(String[] args) {
+        
+        // Tabela verdade para OU EXCLUSIVO ( ^ )
+        System.out.println("\nTABELA VERDADE PARA OU ^ EXCLUSIVO");
+        System.out.println("\t  P     Q     P # Q");
+        System.out.println("\ttrue  true    false");
+        System.out.println("\ttrue  false   true");
+        System.out.println("\tfalse true    true");
+        System.out.println("\tfalse false   false");
+        System.out.println("\t*******************");
+    } // fim main
+} // fim classe
