4bd464dff70ed720f669839e9b20572a729355bd
PedroFilho <psfilho81@hotmail.com>
2021-11-28 19:27:05 -0300

    Create ProdutoRaizQuadrada13.java

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_2/ProdutoRaizQuadrada13.java
@@ -0,0 +1,10 @@
+public class ProdutoRaizQuadrada13 {
+    /*
+        1.2.13 Avalie a expressão (Math.sqrt (2) * Math.sqrt (2) == 2).
+    */
+    public static void main(String[] args) {
+        
+        System.out.println("(Math.sqrt (2) * Math.sqrt (2) == 2) = " +
+                            (Math.sqrt (2) * Math.sqrt (2) == 2 ));  
+    } // fim main    
+} // fim class
