2eb35d8550939deb4e1df110bc4241d3a7f5da45
PedroFilho <psfilho81@hotmail.com>
2021-11-30 11:08:36 -0300

    Update LoopDoWhile18.java

--- CienciaDaComputacao/Capitulo01/Exemplos1_3/LoopDoWhile18.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/LoopDoWhile18.java
@@ -1,33 +1,28 @@
 package Exemplos1_3;
 
-import java.util.Scanner;
-
 public class LoopDoWhile18 {
 
     /*
-        Imprime um ponto escolhido uniformemente ao acaso dentro do círculo centrado
-        em (0, 0) e de raio 1.
-        30/11/2021
-    */
+     * Imprime um ponto escolhido uniformemente ao acaso dentro do círculo centrado
+     * em (0, 0) e de raio 1.
+     * 30/11/2021
+     */
 
-    public static void main(String[] args) { 
-        
-        // declara o objeto entrada para a classe Scanner e inicializa
-        Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) {
 
         // variáveis
         double x, y;
 
         do {
 
-           // Escala x e y para ser aleatório em (-1, 1).
+            // Escala x e y para ser aleatório em (-1, 1).
             x = 2.0 * Math.random() - 1.0;
             y = 2.0 * Math.random() - 1.0;
 
-        } while (x*x + y*y > 1.0);
+        } while (x * x + y * y > 1.0);
 
         // print (x, y)
         System.out.println("(" + x + "," + y + ")");
 
-    } // fim main    
+    } // fim main
 } // fim classe
