ed5446b4681e118017171d1bb8f70c90649512f4
PedroFilho <psfilho81@hotmail.com>
2021-11-30 08:55:13 -0300

    Soma de Aleatórios

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_2/SomaInteiros20.java
@@ -0,0 +1,13 @@
+package Exercicios1_2;
+
+public class SomaInteiros20 {
+    public static void main(String[] args) {
+
+        double a = 1 + Math.random() * 6;
+        double b = 1 + Math.random() * 6;
+        double soma = a + b;
+
+        System.out.println("A soma entre " + (int) a + " + " + (int) b + " = " + (int) soma);
+
+    } // fim main
+} // fim classe
