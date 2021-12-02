47c6e6a8833aa813c215f1f5ad326c06053ddad7
PedroFilho <psfilho81@hotmail.com>
2021-12-02 10:33:49 -0300

    Imprime Olá Mundo"

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_3/ImprimeOlaMundo08.java
@@ -0,0 +1,23 @@
+package Exercicios1_3;
+
+public class ImprimeOlaMundo08 {
+    public static void main(String[] args) {
+
+        // imprime
+        System.out.println("1st Olá, Mundo!");
+        System.out.println("2nd Olá, Mundo!");
+        System.out.println("3rd Olá, Mundo!");
+
+        // variável
+        int contar = 4;
+
+        // enquanto contar menor que 10 faça
+        while (contar <= 10) {
+            // imprime
+            System.out.println(contar + "th Olá, Mundo!");
+            contar += 1;
+
+        } // fim while
+
+    } // fim main
+} // fim classe
