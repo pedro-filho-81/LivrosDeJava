3cf81648dac51954232a41207e227f1a8b34ae23
PedroFilho <psfilho81@hotmail.com>
2021-12-03 16:17:18 -0300

    Atualização

--- CienciaDaComputacao/Capitulo01/Exemplos1_4/InicializandoVetor02.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_4/InicializandoVetor02.java
@@ -4,25 +4,31 @@ public class InicializandoVetor02 {
     public static void main(String[] args) {
 
         // declarando e inicializando vetor
+        // tipo-nome= inicializa [10] quantidade de elementos
+        // tipo double, nome x, novo double[tamanho => 10] 
         double[] x = new double[10];
         double[] y = new double[10];
 
         // criando variáveis
         double soma = 0.0;
 
-        // adicionando valores a x, y e soma
+        // adicionando valores aos vetores x e y
         for (int i = 0; i < 10; i++) {
             x[i] = Math.random() * 30; // adiciona valores a x
             y[i] = Math.random() * 20; // adiciona valores a y
-
         } // fim for
 
-        // loop para mostrar os valores de x e y
+        // cabeçalho
+        System.out.println("\tx[i]\ty[i]\tx[i]*y[i]\tSoma");
+
+        // loop para somar os valores de x e y
         for (int i = 0; i < 10; i++) {
             
             soma += x[i] * y[i]; // soma os produtos de x e y
-            System.out.println(x[i] + "    " + y[i] + "    " +
-                                 x[i] * y[i] + "    " + soma);
+
+            // depois imprimir o resultado
+            System.out.println("\t " + (int) x[i] + "\t " + (int) y[i] + "\t   " +
+                                 (int) x[i] * (int) y[i] + "\t         " + (int) soma);
         } // fim for
     } // fim main
 }// fim classe
