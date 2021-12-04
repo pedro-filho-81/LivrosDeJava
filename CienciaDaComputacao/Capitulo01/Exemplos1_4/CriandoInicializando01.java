79cbf219eef19e8709088db511ecc604437e52b2
PedroFilho <psfilho81@hotmail.com>
2021-12-04 11:55:54 -0300

    Update CriandoInicializando01.java

--- CienciaDaComputacao/Capitulo01/Exemplos1_4/CriandoInicializando01.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_4/CriandoInicializando01.java
@@ -5,8 +5,7 @@ public class CriandoInicializando01 {
     public static void main(String[] args) {
         
         // criando e inicializando um vetor
-        int[] numero;           //declarando um vetor
-        
+        int[] numero;           //declarando um vetor        
         numero = new int[ 10 ]; // inicializando um vetor
 
         // loop for para adicionar valores ao vetor
