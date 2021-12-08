248821370d74c31ae4fbf08fe565142e02134dee
PedroFilho <psfilho81@hotmail.com>
2021-12-08 11:01:23 -0300

    Olá Mundo Java

---
+++ ComoProgramar/Capitulo02/ExemplosCap02/OlaMundo01.java
@@ -0,0 +1,9 @@
+package ExemplosCap02;
+
+class OlaMundo01 {
+    public static void main(String[] args) {
+
+        // imprime na tela
+        System.out.println("Olá, Mundo Java!");
+    } // fim main
+} // fim classe
