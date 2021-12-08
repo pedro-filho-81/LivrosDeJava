cf8c5ad575028d12b1a6ce58cb4a1ec67612d045
PedroFilho <psfilho81@hotmail.com>
2021-12-08 11:30:32 -0300

    ATUALIZAÇÃO

--- ComoProgramar/Capitulo02/ExemplosCap02/Addition.java
+++ ComoProgramar/Capitulo02/ExemplosCap02/Addition.java
@@ -1,12 +1,11 @@
+
 // Fig. 2.7: Addition.java
 // Addition program that displays the sum of two numbers.
 import java.util.Scanner; // program uses class Scanner
 
-public class Addition 
-{
+public class Addition {
    // main method begins execution of Java application
-   public static void main(String[] args)
-   {
+   public static void main(String[] args) {
       // create a Scanner to obtain input from the command window
       Scanner input = new Scanner(System.in);
 
@@ -14,10 +13,10 @@ public class Addition
       int number2; // second number to add
       int sum; // sum of number1 and number2
 
-      System.out.print("Enter first integer: "); // prompt 
+      System.out.print("Enter first integer: "); // prompt
       number1 = input.nextInt(); // read first number from user
 
-      System.out.print("Enter second integer: "); // prompt 
+      System.out.print("Enter second integer: "); // prompt
       number2 = input.nextInt(); // read second number from user
 
       sum = number1 + number2; // add numbers, then store total in sum
@@ -27,16 +26,16 @@ public class Addition
 } // end class Addition
 
 /**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and *
+ * Pearson Education, Inc. All Rights Reserved. *
+ * *
+ * DISCLAIMER: The authors and publisher of this book have used their *
+ * best efforts in preparing the book. These efforts include the *
+ * development, research, and testing of the theories and programs *
+ * to determine their effectiveness. The authors and publisher make *
+ * no warranty of any kind, expressed or implied, with regard to these *
  * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * and publisher shall not be liable in any event for incidental or *
+ * consequential damages in connection with, or arising out of, the *
+ * furnishing, performance, or use of these programs. *
  *************************************************************************/
--- ComoProgramar/Capitulo02/ExemplosCap02/OlaMundo01.java
+++ ComoProgramar/Capitulo02/ExemplosCap02/OlaMundo01.java
@@ -1,6 +1,8 @@
 package ExemplosCap02;
 
+// cria a classe OlaMundo01
 class OlaMundo01 {
+    // função principal
     public static void main(String[] args) {
 
         // imprime na tela
