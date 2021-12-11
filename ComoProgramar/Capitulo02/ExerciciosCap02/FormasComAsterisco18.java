19ed11f555374735452b35e99f1b450e49d9c5b5
PedroFilho <psfilho81@hotmail.com>
2021-12-11 17:12:06 -0300

    Exibe Formas com Asterisco

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/FormasComAsterisco18.java
@@ -0,0 +1,22 @@
+public class FormasComAsterisco18 {
+    /*
+        2.18 (Exibindo formas com asteriscos) Escreva um aplicativo 
+        que exiba uma caixa, uma elipse, uma seta 
+        e um losango utilizando asteriscos 
+        Pedro, 11/12/2021
+    */
+
+    public static void main(String[] args) {
+        
+        System.out.println("*********      ***       *       *");
+        System.out.println("*       *    *     *    ***     * *");
+        System.out.println("*       *   *       *  *****   *   *");
+        System.out.println("*       *   *       *    *    *     *");
+        System.out.println("*       *   *       *    *   *       *");
+        System.out.println("*       *   *       *    *    *     *");
+        System.out.println("*       *   *       *    *     *   *");
+        System.out.println("*       *    *     *     *      * *");
+        System.out.println("*********      ***       *       *");
+        
+    } // fim main    
+} // fim classe
