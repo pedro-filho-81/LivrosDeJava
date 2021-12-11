44cc4e061b315c4ce1e3b7b8e64db667bccf45a4
PedroFilho <psfilho81@hotmail.com>
2021-12-11 17:18:45 -0300

    Create UsandoPrintF19.java

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/UsandoPrintF19.java
@@ -0,0 +1,13 @@
+public class UsandoPrintF19 {
+    /*
+     * 2.19 O que o seguinte código imprime?
+     * System.out.printf("*%n**%n***%n****%n*****%n");
+     * 11/12/2021
+     */
+
+    public static void main(String[] args) {
+        // o que será mostrado?
+        System.out.printf("*%n**%n***%n****%n*****%n");
+
+    } // fim main
+} // fim classe
