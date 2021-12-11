7791b17bb6cb944008fe6b644bed33cc31e4b015
PedroFilho <psfilho81@hotmail.com>
2021-12-11 17:35:13 -0300

    Create UsandoPrintLn20.java

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/UsandoPrintLn20.java
@@ -0,0 +1,18 @@
+public class UsandoPrintLn20 {
+    /*
+        2.20 O que o seguinte código imprime?
+            System.out.println("*"); 
+            System.out.println("***"); 
+            System.out.println("*****"); 
+            System.out.println("****"); 
+            System.out.println("**");
+    */
+    public static void main(String[] args) {
+        
+        System.out.println("*"); 
+        System.out.println("***"); 
+        System.out.println("*****"); 
+        System.out.println("****"); 
+        System.out.println("**");
+    } // fim main
+} // fim classe
