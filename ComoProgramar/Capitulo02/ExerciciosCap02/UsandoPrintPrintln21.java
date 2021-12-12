92e747a0744956b062f218021eecc54447ed3aff
PedroFilho <psfilho81@hotmail.com>
2021-12-12 16:25:14 -0300

    Usando Printe e Printlh

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/UsandoPrintPrintln21.java
@@ -0,0 +1,19 @@
+public class UsandoPrintPrintln21 {
+    /*
+        2.21 O que o seguinte código imprime?
+            System.out.print("*"); 
+            System.out.print("***"); 
+            System.out.print("*****"); 
+            System.out.print("****"); 
+            System.out.println("**");
+    */
+    public static void main(String[] args) {
+        
+        // imprima um ao lado do outro;
+        System.out.print("*"); 
+        System.out.print("***"); 
+        System.out.print("*****"); 
+        System.out.print("****"); 
+        System.out.println("**");
+    } // fim main
+} // fim classe
