e25cccc59b32ce574bd14df195b9c8f531e1cff0
PedroFilho <psfilho81@hotmail.com>
2021-12-12 16:25:27 -0300

    Create Exercicio22.java

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/Exercicio22.java
@@ -0,0 +1,20 @@
+public class Exercicio22 {
+    /*
+        2.22 O que o seguinte código imprime?
+            System.out.print("*"); 
+            System.out.println("***"); 
+            System.out.println("*****"); 
+            System.out.print("****"); 
+            System.out.println("**");
+    */
+    
+    public static void main(String[] args) {
+        
+        // imprime
+        System.out.print("*"); 
+        System.out.println("***"); 
+        System.out.println("*****"); 
+        System.out.print("****"); 
+        System.out.println("**");
+    } // fim main
+} // fim classe
