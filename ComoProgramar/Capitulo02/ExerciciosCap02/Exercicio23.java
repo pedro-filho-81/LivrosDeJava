0b091d4ce2258ec9e626c427ad2e11b5a5b07353
PedroFilho <psfilho81@hotmail.com>
2021-12-12 16:25:35 -0300

    Create Exercicio23.java

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/Exercicio23.java
@@ -0,0 +1,11 @@
+public class Exercicio23 {
+    /*
+        2.23 O que o seguinte código imprime?
+            System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+    */
+    public static void main(String[] args) {
+        
+        // imprime
+        System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+    } // fim main    
+} // fim classe
