f179d2ad32937ceb75048751fb16c34644766f8e
PedroFilho <psfilho81@hotmail.com>
2021-12-13 11:36:32 -0300

    Tabela de Quadrados e Cubos

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/TabelaQuadradosCubos31.java
@@ -0,0 +1,24 @@
+public class TabelaQuadradosCubos31 {
+    /*
+     * (Tabela de quadrados e cubos) Utilizando apenas as técnicas de programação
+     * que aprendeu neste capítulo, escreva um aplicativo que
+     * calcule os quadrados e cubos dos números de 0 a 10 e imprima os valores
+     * resultantes em formato de tabela como a seguir
+     * Pedro, 13/12/2021
+     */
+    public static void main(String[] args) {
+
+        // imprime cabeçalho
+        System.out.printf("%s%13s%10s%n", "Númeero", "Quadrado", "Cubo");
+        System.out.printf("%5d%13d%12d%n", 1, 1 * 1, 1 * 1 * 1);
+        System.out.printf("%5d%13d%12d%n", 2, 2 * 2, 2 * 2 * 2);
+        System.out.printf("%5d%13d%12d%n", 3, 3 * 3, 3 * 3 * 2);
+        System.out.printf("%5d%13d%12d%n", 4, 4 * 4, 4 * 4 * 4);
+        System.out.printf("%5d%13d%12d%n", 5, 5 * 5, 5 * 5 * 5);
+        System.out.printf("%5d%13d%12d%n", 6, 6 * 6, 6 * 6 * 6);
+        System.out.printf("%5d%13d%12d%n", 7, 7 * 7, 7 * 7 * 7);
+        System.out.printf("%5d%13d%12d%n", 8, 8 * 8, 8 * 8 * 8);
+        System.out.printf("%5d%13d%12d%n", 9, 9 * 9, 9 * 9 * 9);
+        System.out.printf("%5d%13d%12d%n", 10, 10 * 10, 10 * 10 * 10);
+    } // fim main
+} // fim classe
