8717d0bbb9076092a2bb6bcefdb824b22a5392a5
PedroFilho <psfilho81@hotmail.com>
2021-12-13 10:15:35 -0300

    Tabuleiro com Asterisco

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/TabuleiroComAsterisco27.java
@@ -0,0 +1,21 @@
+public class TabuleiroComAsterisco27 {
+    /*
+        2.27 (Padrão de tabuleiro de damas de asteriscos) 
+        Escreva um aplicativo que exiba um padrão de tabuleiro de damas, 
+        como mostrado a seguir:
+        Pedro, 13/12/2021
+    */
+
+    public static void main(String[] args) {
+        
+        System.out.println("* * * * * * * * * *");
+        System.out.println(" * * * * * * * * * *");
+        System.out.println("* * * * * * * * * *");
+        System.out.println(" * * * * * * * * * *");
+        System.out.println("* * * * * * * * * *");
+        System.out.println(" * * * * * * * * * *");
+        System.out.println("* * * * * * * * * *");
+        System.out.println(" * * * * * * * * * *");
+        
+    } // fim main    
+} // fim classe
