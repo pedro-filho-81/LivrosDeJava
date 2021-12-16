728ec149901da89ce31402d0763e3e3c05549c69
PedroFilho <psfilho81@hotmail.com>
2021-12-16 10:48:44 -0300

    Desenha um X na Janela

---
+++ ComoProgramar/Capitulo04/ExemplosCap04/DesenharLinhas05/DesenharLInhasTeste05.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+public class DesenharLInhasTeste05 {
+    public static void main(String[] args) {
+        
+        // cria um painel que conem nosso desenho
+        DesenharLinhas05 painel = new DesenharLinhas05();
+
+        // cria um novo quadro praa armazenar o paines
+        JFrame aplicacao = new JFrame();
+
+        // Configura o frame para ser enerrado quendo o frame for fechado
+        aplicacao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        aplicacao.add(painel); // adiciona a aplicação
+        aplicacao.setSize(250, 250); // configura o tamanho em máximo
+        aplicacao.setVisible(true); // mantem a aplicação
+    } // fim main
+} // fim classe
---
+++ ComoProgramar/Capitulo04/ExemplosCap04/DesenharLinhas05/DesenharLinhas05.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+// desenha um X a partir dos cantos da janela
+public class DesenharLinhas05 extends JPanel {
+    // desenha um   
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // variáveis largura e altura
+        int largura = getWidth(); // largura do quadro
+        int altura = getHeight(); // altura do quadro
+
+        // desenha uma linha a partir do canto superior esquerdo (0, 0) até o canto
+        // inferior direito
+        g.drawLine( 0, 0, largura, altura);
+
+        // desenha uma linha a partir do canto inferior esquerdo até o canto superior direito
+        g.drawLine(0, altura, largura, 0);
+
+    } // fim paintComponent
+} // fim class Desenhar
