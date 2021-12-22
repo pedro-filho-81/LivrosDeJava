c51fb71df16d77f21a90cd9304d08ea9c1fbfafe
PedroFilho <psfilho81@hotmail.com>
2021-12-22 09:14:54 -0300

    Soma de x

---
+++ ComoProgramar/Capitulo04/ExerciciosCap04/Exercicio4_5/src/exercicio4_5/Exercicio4_5.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio4_5;
+
+/**
+ *4.5 Escreva uma instrução Java para realizar cada uma das seguintes tarefas:
+a) Declarar variáveis sum do tipo int e inicialize-as como 0.
+b) Declarar variáveis x do tipo int e inicialize-as como 0.
+c) Adicione a variável x à variável sum, e atribua o resultado à variável sum.
+d) Imprima "A soma é: " seguido pelo valor da variável sum.
+ * @author Pedro Filho, 22/12/2021
+ */
+public class Exercicio4_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic her
+        
+        // a) Declarar variáveis sum do tipo int e inicialize-as como 0.
+        int soma = 0;   
+        // b) Declarar variáveis x do tipo int e inicialize-as como 0.
+        int x = 0;
+        //c) Adicione a variável x à variável sum, e atribua o resultado à variável sum.
+          soma = x;
+        //d) Imprima "A soma é: " seguido pelo valor da variável sum.
+        System.out.println("A soma é " + soma);
+    } // fim main
+}// fim classe
