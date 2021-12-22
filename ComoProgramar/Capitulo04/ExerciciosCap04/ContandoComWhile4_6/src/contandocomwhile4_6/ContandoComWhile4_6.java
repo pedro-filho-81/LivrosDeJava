38e1fa8d83d24151dab941d46b0a1cd9ffa44d30
PedroFilho <psfilho81@hotmail.com>
2021-12-22 09:27:42 -0300

    Exercício4_6

---
+++ ComoProgramar/Capitulo04/ExerciciosCap04/ContandoComWhile4_6/src/contandocomwhile4_6/ContandoComWhile4_6.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contandocomwhile4_6;
+
+/**
+ *  4.6 Combine as instruções escritas no Exercício 4.5 em um aplicativo Java que calcula 
+ * e imprime a soma dos inteiros de 1 a 10. Utilize a instrução while.  
+ * @author Pedro Filho, 22/12/2021
+ */
+
+public class ContandoComWhile4_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // variáveis
+        int contar = 1;
+        int soma = 0;
+        
+        while( contar < 11 ) {
+            // imprime
+            System.out.print( contar + " " );
+            soma += contar;
+            contar++; // incremento
+        } // fim while
+        
+        // resultado
+        System.out.println("\nA soma é " + soma);
+    } // fim main
+    
+} // fim classe
