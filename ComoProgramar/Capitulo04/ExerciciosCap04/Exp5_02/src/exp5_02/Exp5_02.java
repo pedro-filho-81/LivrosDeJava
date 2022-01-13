135fd77920d8cf24186c964121839107a1acd3e5
PedroFilho <psfilho81@hotmail.com>
2022-01-13 11:19:57 -0300

    Repetição com For

---
+++ ComoProgramar/Capitulo04/ExerciciosCap04/Exp5_02/src/exp5_02/Exp5_02.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exp5_02;
+
+/**
+ * Repetição controlado por contador com a instrução for
+ * @author 13/01/2022
+ */
+public class Exp5_02 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // loop for para contar até 10
+        for( int contador = 1; contador <= 10; contador ++ ) {
+            System.out.printf("%d ", contador ); // imprime até 10
+        } // fim for
+        
+        System.out.println(); // pula linha
+    } // fim main    
+} // fim classe
