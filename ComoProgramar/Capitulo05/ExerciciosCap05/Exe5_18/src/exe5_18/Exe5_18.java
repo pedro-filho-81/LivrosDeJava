a832e0b8f1a674c95da77bdc3b39daedad09a222
PedroFilho <psfilho81@hotmail.com>
2022-01-20 17:12:12 -0300

    Juros Compostos com Inteiros

---
+++ ComoProgramar/Capitulo05/ExerciciosCap05/Exe5_18/src/exe5_18/Exe5_18.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe5_18;
+
+/**
+ *5.18 (Programa de juros compostos modificado) Modifique o aplicativo na Figura 5.6 para utilizar apenas 
+ * inteiros para calcular os juros compostos. [Dica: trate todas as quantidades monetárias como números inteiros 
+ * em centavos. Então, divida o resultado em suas partes dólar e centavos utilizando as operações divisão e resto, 
+ * respectivamente. Insira uma vírgula entre as partes dólar e centavos.]
+ * @author Pedro Filho, 20/01/2022
+ */
+public class Exe5_18 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // variáveis
+        int principal;
+        int centPrincipal;
+        int tempo;
+        int taxa;
+        int montante;
+        int centMontante;
+        
+        
+    }
+    
+}
