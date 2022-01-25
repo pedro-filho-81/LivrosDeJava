2ce729aa077f4d392df0c17d9d12d740a3797910
PedroFilho <psfilho81@hotmail.com>
2022-01-25 18:02:12 -0300

    Métodos

---
+++ ComoProgramar/Capitulo06/ExerciciosCap06/Exe6_04/src/exe6_04/Exe6_04.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe6_04;
+
+/**
+ *  6.4 Forneça o cabeçalho de método para cada um dos seguintes métodos.
+a) O método hypotenuse, que aceita dois argumentos de ponto flutuante de precisão dupla side1 e side2 
+* e retorna um resultado de ponto flutuante de dupla precisão.
+b) O método smallest, que recebe três inteiros x, y e z e retorna um inteiro.
+c) O método instructions, que não aceita nenhum argumento e não retorna um valor. 
+* [Observação: esses métodos são comumente utilizados para exibição de instruções para o usuário.]
+d) O método intToFloat, que recebe um argumento number do tipo inteiro e retorna um float
+ * @author Pedro, 25/01/2022
+ */
+public class Exe6_04 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // variáveis
+        double hipot = hypotenuse(5, 9);
+        double small;
+        
+        System.out.printf("hipotenuse = %.2f%n", hipot );
+    } // fim main
+
+        // 6.4 Forneça o cabeçalho de método para cada um dos seguintes métodos.
+           // a) O método hypotenuse, que aceita dois argumentos de ponto flutuante de precisão dupla side1 e side2 e retorna um resultado de 
+          // ponto flutuante de dupla precisão.
+                public static double hypotenuse( double side1, double side2) {
+                    double hipo = Math.pow(side1, 2) + Math.pow(side2, 2);
+                    return hipo;
+                } // fim hipotenuse
+            
+            // b) O método smallest, que recebe três inteiros x, y e z e retorna um inteiro.
+                    public static int smallest( int x, int y, int z ) {
+                        int sma = x; // sma recebe x
+                        
+                        if( y > sma) // se y maior que sma
+                            sma = y;
+                                    
+                       if( z > sma )
+                           sma = z;
+                                                
+                        return sma;
+                    } // fim método
+                            
+           // c) O método instructions, que não aceita nenhum argumento e não retorna um valor. [Observação: esses métodos são comumente 
+            // utilizados para exibição de instruções para o usuário.]
+                    public static void instruction() {
+                        System.out.println("colocsr instruções aqui");
+                    } // fim métoro
+                    
+            // d) O método intToFloat, que recebe um argumento number do tipo inteiro e retorna um float    
+                    public static float intToFloat( int x ) {
+                                    
+                        System.out.printf("int x = %d%n", x);
+                        
+                        float numero = (float) x;
+                        
+                        System.out.printf("float numero = %.2f%n", numero );
+                        
+                        return numero;
+                        
+                    } // fim método
+                    
+} // fim classe
