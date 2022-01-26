0b8728a2abc56d84ef69f92f139a7f7001a6e3de
PedroFilho <psfilho81@hotmail.com>
2022-01-26 10:58:10 -0300

    Método

---
+++ ComoProgramar/Capitulo06/ExerciciosCap06/Exe6_05/src/exe6_05/Exe6_05.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exe6_05;
+
+/**
+ *
+ * @author samsung
+ */
+public class Exe6_05 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        g();
+        h();
+        
+    } // main
+    
+        static void g() 
+        { 
+            System.out.println("Inside method g"); 
+        }
+                   
+        static void h() 
+        { 
+            System.out.println("Inside method h"); 
+        }
+
+}
