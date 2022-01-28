e8895ef968f53875391d5d0cb646238965a3597c
PedroFilho <psfilho81@hotmail.com>
2022-01-28 18:23:04 -0300

    Frequência com array

---
+++ ComoProgramar/Capitulo07/ExemplosCap07/Exp7_07/src/exp7_07/Exp7_07.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exp7_07;
+
+/**
+ *  Vinte estudantes foram solicitados a classificar em uma escala de 1 a 5 
+ * a qualidade da comida no refeitório estudantil, com 1 sendo “horrível” e 
+ * 5 sendo “excelente”. Coloque as 20 respostas em um array de inteiros e 
+ * determine a frequência de cada classificação
+ * @author 28/01/2022
+ */
+
+public class Exp7_07 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // cria array de resposta
+        int[] respostas = { 1, 2, 5, 4, 3, 5, 2, 1, 3, 3, 1, 14, 3, 3, 3, 2, 3, 3, 2, 4 };
+        
+        int[] frequencia = new int[ 6 ];
+        
+        //para cada resposta, seleciona um elemento de respostas e utiliza esse valor
+        // como índice de frequência para determinar o elemento e incrementar
+        for( int resposta = 0; resposta < respostas.length; resposta++ ) {
+            try
+            {
+                ++frequencia[ respostas[ resposta ] ];
+            } // fim try
+            catch(ArrayIndexOutOfBoundsException e )
+            {
+                System.out.println( e ); // invoca o método toString
+                System.out.println("Índice do vetor fora dos limites da exceção");
+                System.out.printf("respostas[ %d ] = %d%n%n", resposta, respostas[ resposta ] );
+            } // fim catch
+        } // fim for resposta
+        
+        System.out.printf("%s%15s%n", "Índice", "Frequência");
+        
+        // gera a saída  dos valoresdo vetor
+        for( int indice = 1; indice < frequencia.length; indice++ )
+            System.out.printf("%6d%10d%n", indice,  frequencia[indice] );
+    } // fim main    
+} // fim classe
