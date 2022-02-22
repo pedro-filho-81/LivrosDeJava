57298fb13b253b76fe69831e59e22a3f3ca5c293
PedroFilho <psfilho81@hotmail.com>
2022-02-22 20:13:38 -0300

    Contar

---
+++ ComoProgramar/Capitulo07/ExerciciosCap07/Vetores/src/vetoresdiversos/Contar.java
@@ -0,0 +1,12 @@
+package vetoresdiversos;
+
+public class Contar {
+
+    // arrays
+    int[] contar = new int[ 10 ];
+    
+    for( int i = 0; i < 10; i++ ) {
+        contar[ i ] = 0;
+    } // fim for
+        
+} // fim classe contar
