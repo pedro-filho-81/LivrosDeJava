568ee532b16ad6c560a47fd83a0fe24a559860e7
PedroFilho <psfilho81@hotmail.com>
2022-02-23 12:09:14 -0300

    Lista Com  Comprimento Variável

---
+++ JavaTheCompleteReference/Capitulo02/ExemplosCap02/ListaComprimentoVariavel/src/listacomprimentovariavel/ListaComprimentoVariavel.java
@@ -0,0 +1,18 @@
+
+package listacomprimentovariavel;
+
+/**
+ *7.14 (Lista de argumento de comprimento variável) Escreva um aplicativo que calcula 
+ * o produto de uma série de inteiros que são passados para método product utilizando 
+ * uma lista de argumentos de comprimento variável. Teste seu método com várias chamadas, 
+ * cada uma com um número diferente de argumentos.
+ * @author Pedro, 23/02/2022
+ */
+public class ListaComprimentoVariavel {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    } // fim main    
+} // fim classe
