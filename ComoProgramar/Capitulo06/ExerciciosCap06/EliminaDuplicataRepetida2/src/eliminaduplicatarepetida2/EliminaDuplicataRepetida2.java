5b6b941bc966c97b9555a841c0caa0bb34f9d0a1
PedroFilho <psfilho81@hotmail.com>
2022-02-26 18:01:02 -0300

    Elimina Duplicatas 2

---
+++ ComoProgramar/Capitulo06/ExerciciosCap06/EliminaDuplicataRepetida2/src/eliminaduplicatarepetida2/EliminaDuplicataRepetida2.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eliminaduplicatarepetida2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Elimina duplicata repetida 2 usando a classe Arrays
+ * @author Pedro, 24/02/2022
+ */
+public class EliminaDuplicataRepetida2 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // cria o objeto array list
+        ArrayList<Integer> duplicatas = new ArrayList<Integer>();
+        
+        // cria um objeto da classe Scanner
+        Scanner entrada = new Scanner(System.in);
+        
+        // variável
+        int numero = 0;
+        
+        // loop para adicionar valores ao vetor
+        while( numero != -1 ) {
+          
+            // entrada de dados
+            System.out.print("Digite um inteiro ( -1 para sair ): ");
+            // aguarda entrada do usuário
+            numero = entrada.nextInt();
+            
+            // se umero igual a -1
+            if( numero == -1 )
+                // sair do loop
+                break;
+            
+            // verifica se o número digitado consta no vetor
+            if( duplicatas.contains(numero) == false ) // se não
+                // adiciona o número ao vetor
+                duplicatas.add(numero);
+            else 
+                System.out.println("Repetido");
+                                
+        } // fim while
+        
+        mostrarValores(duplicatas);
+        
+        // pula linha
+        System.out.println();     
+        
+    } // fim main
+
+    // cria método para mostrar os valores do vetor
+    public static void mostrarValores( ArrayList<Integer> duplicatas ) {
+        
+        System.out.print("duplicatas = { ");
+        
+        for (Integer duplicata : duplicatas) {
+            // imprime o valor
+            System.out.printf("%s ", duplicata );
+        }   // fim for
+        
+        System.out.println("};");
+            
+    } // fim método mostrar valores
+
+} // fim classe
