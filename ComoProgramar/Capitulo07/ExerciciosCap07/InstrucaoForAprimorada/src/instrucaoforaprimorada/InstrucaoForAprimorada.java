4bb9411eb571b7164028c409feae88a589dec183
PedroFilho <psfilho81@hotmail.com>
2022-03-03 18:24:20 -0300

    Update InstrucaoForAprimorada.java

--- ComoProgramar/Capitulo07/ExerciciosCap07/InstrucaoForAprimorada/src/instrucaoforaprimorada/InstrucaoForAprimorada.java
+++ ComoProgramar/Capitulo07/ExerciciosCap07/InstrucaoForAprimorada/src/instrucaoforaprimorada/InstrucaoForAprimorada.java
@@ -25,15 +25,15 @@ public class InstrucaoForAprimorada {
         
         // entrada de dados
         System.out.print("Informe o tamanho do vetor: ");
-        int tamanho = entrada.nextInt();
+        int tamanho = entrada.nextInt(); // usuário informa o tamanho do vetor
         
-        // declara um vetor
+        // declara um vetor e determina o seu tamanho
         double[] vetor = new double[tamanho ];
         
-        // chama método adicionar valor
+        // chama método adicionar valor ao vetor
         adicionarValor(vetor, tamanho );
     
-        // chama método mostrar valor
+        // chama método mostrar valor do vetor
         mostrarValor(vetor);
     } // fim main
     
@@ -42,45 +42,66 @@ public class InstrucaoForAprimorada {
         
         // cria o objeto Scanner
         Scanner entrada = new Scanner(System.in);
+        
+        // variável para o indice do vetor
         int indice = 0; // para o índice do vetor
         
-        // loop for aprimorado para atribuir valor ao vetor
+        // loop while enquanto o indice for menor que o tamanho do vetor faça
         while( indice < tamanho ) {
             
+            // entrada de dados
             System.out.print("Digite um valor de ponto-flutuante: ");
+            // aguarda a entrada do valor do elemento do vetor pelo uauário
             double valor = entrada.nextDouble();  
             
+            // cria a variável resposta que 
+            // chama o método pasquisar valor que recebe como argumentos
+            // o vetor, o valor do usuário e o tamanho do vetor
+            // o método verifica se o valor já existe no vetor
+            // se exitir é retornado o valor -1
+            // se não existir o valor ele é adicionado ao vetor
             double resposta = pesquisarValor(vetor, valor, tamanho);
         
+            // cria um loop infinito
             while( true ) {
-                if( resposta == -1 ) {
+                
+                // verifica se a resposta é igual a -1
+                if( resposta == -1 ) { // se verdade
+                    // é impresso a mensagem
                     System.out.println("Valor repetido;");
-                    break;
+                    break; // e sai do loop infinito
                 } // fim if
-                else {
+                else { // se a resposta for diferente de -1
+                    // o valor digitado é adicionado ao vetor 
                     vetor[ indice ] = resposta;
+                    // e ao índice é adicionado 1
                     indice++;
-                    break;
+                    break; // e sai do loop infinito
                 } // fim else
-            } // fim while resposta,,,            
-        } // fim while indice 
-        
+                
+            } // fim loop infinito
+        } // fim while indice         
     } // fim adicionar valor
     
     // método pesquisar valor
     public static double pesquisarValor( double[] vetor, double valor, int tamanho ) {
         
-        int indice = 0;
+        // cria o indice do vetor
+        int indice = 0; //  começando em 0
         
+        // loop enquanto o indice for menor que o tamanho faça
         while( indice < tamanho) {
         
-            if( vetor[ indice ] == valor ) {
+            // verifica no vetor se existe um valor igual ao digitado pelo usuário
+            if( vetor[ indice ] == valor ) { // se existir
+                // retorna o valor -1.0 e sai do método
                 return -1.0;
             } // fim if
-            
-            indice++;
+            else // se não existir
+                indice++; // incrementa o indice em 1
         } // fim while
         
+        // e retorna o valor digitado
         return valor;
     } // fim pesquisar valor
     
@@ -88,8 +109,9 @@ public class InstrucaoForAprimorada {
     public static void mostrarValor( double[] vetor) {
     
         System.out.print("vetor = {");
-        
+        // loop com o for aprimorado para mostrar os valores do vetor
         for( double valor : vetor ) {
+            // imprime os valores do vetor
             System.out.printf("%5.2f ", valor );
         } // fim for aprimorado
         
