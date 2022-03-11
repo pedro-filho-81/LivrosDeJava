f5d3ecead632e64f8ae2490b1f925038333a8a19
PedroFilho <psfilho81@hotmail.com>
2022-03-11 17:13:49 -0300

    Declara e Cria uma Matriz

---
+++ ComoProgramar/Capitulo07/ExerciciosCap07/Exercicio7_9/src/exercicio7_9/Exercicio7_9.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio7_9;
+
+/**
+ *  7.9 Considere um array de inteiros dois por três t.
+ * a) Escreva uma instrução que declara e cria t.
+ * b) Quantas linhas tem t?
+c) Quantas colunas tem t?
+d) Quantos elementos tem t?
+e) Escreva expressões de acesso para todos os elementos na linha 1 de t.
+f) Escreva expressões de acesso para todos os elementos na coluna 2 de t.
+g) Escreva uma única instrução que configura o elemento de t na linha 0 e na coluna 1 como zero.
+h) Escreva instruções individuais para inicializar cada elemento de t para zero.
+i) Escreva uma instrução for aninhada que inicializa cada elemento de t como zero.
+j) Escreva uma instrução for aninhada que insere os valores para os elementos de t a partir do usuário.
+k) Escreva uma série de instruções que determina e exibe o valor menor em t.
+l) Escreva uma única instrução printf que exibe os elementos da primeira linha de t.
+m) Escreva uma instrução que soma os elementos da terceira coluna de t. Não utilize repetição.
+n) Escreva uma série de instruções que exibe o conteúdo de t no formato tabular. Liste os índices de coluna como títulos na parte superior 
+e liste os índices de linha à esquerda de cada linha.
+ * @author samsung
+ */
+public class Exercicio7_9 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //7.9 Considere um array de inteiros dois por três t.
+        // a) Escreva uma instrução que declara e cria t.
+        int[][] matriz = new int[2][3];
+        
+       //b) Quantas linhas tem t?
+        System.out.println("A matriz tem 2 linhas.");
+        //c) Quantas colunas tem t?
+        System.out.println("E 3 colunas.");
+        
+/*        d) Quantos elementos tem t?
+        e) Escreva expressões de acesso para todos os elementos na linha 1 de t.
+        f) Escreva expressões de acesso para todos os elementos na coluna 2 de t.
+        g) Escreva uma única instrução que configura o elemento de t na linha 0 e na coluna 1 como zero.
+        h) Escreva instruções individuais para inicializar cada elemento de t para zero.
+        i) Escreva uma instrução for aninhada que inicializa cada elemento de t como zero.
+        j) Escreva uma instrução for aninhada que insere os valores para os elementos de t a partir do usuário.
+        k) Escreva uma série de instruções que determina e exibe o valor menor em t.
+        l) Escreva uma única instrução printf que exibe os elementos da primeira linha de t.
+        m) Escreva uma instrução que soma os elementos da terceira coluna de t. Não utilize repetição.
+        n) Escreva uma série de instruções que exibe o conteúdo de t no formato tabular. Liste os índices de coluna como títulos na parte superior 
+        e liste os índices de linha à esquerda de cada linha.
+*/
+    } // fim main    
+} // fim classe
