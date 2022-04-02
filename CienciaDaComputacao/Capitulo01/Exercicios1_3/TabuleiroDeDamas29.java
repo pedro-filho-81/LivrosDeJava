46bad720bf9ed47ca2677b689289ad6a6e2790e9
PedroFilho <psfilho81@hotmail.com>
2022-04-02 12:15:10 -0300

    Create TabuleiroDeDamas29.java

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_3/TabuleiroDeDamas29.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class TabuleiroDeDamas29 {
+    /**
+     * Tabuleiro de damas
+     * 1.3.29 Escreva um programa Checkerboard que receba um argumento
+     * de linha de comando inteiro n e use um loop aninhado dentro de
+     * um loop para imprimir um padrão quadridimensional n por n com
+     * espaços e asteriscos alternados.
+     * Sedgewick, Robert; Wayne, Kevin. Ciência da Computação (pág. 85).
+     * Pearson Educação. Edição do Kindle.
+     * 
+     * @author Pedro, 31/03/2022
+     */
+    public static void main(String[] args) {
+
+        // objeto Scanner
+        Scanner input = new Scanner(System.in);
+
+        // entrada de dados
+        System.out.print("Digite um inteiro: ");
+        int valor = input.nextInt();
+
+        for (int i = 0; i < valor; i++) {
+            // imprime 2 espaços
+            System.out.print(" ");
+            // loop for para o asterisco
+            for (int j = 0; j < valor; j++) {
+                // imprime o asterisco
+                System.out.print("* ");
+            } // fim for interno
+              // pula uma linha
+            System.out.println();
+        } // fim for externo
+    } // fim main
+} // fim classe
