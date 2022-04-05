7b3dc8ffe787acf6ea285dfb12dfa92cd4d88a12
PedroFilho <psfilho81@hotmail.com>
2022-04-05 12:26:05 -0300

    Update EncontraUmMes5.java

--- CienciaDaComputacao/Capitulo01/Exemplos1_4/Vetor1_4/EncontraUmMes5.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_4/Vetor1_4/EncontraUmMes5.java
@@ -11,15 +11,19 @@ public class EncontraUmMes5 {
         System.out.print("Digite o mês que deseja ver entre (1 e 12): ");
         int mes = input.nextInt();
 
-        // vetor
+        // declara, cria e inicializa um vetor
         String[] vetorMes = {
                 "Janeiro", "Fevereiro", "Marco", "Abril",
                 "Maio", "Junho", "Julho", "Agosto",
                 "Setembro", "Outubro", "novembro", "Dezembro"
-        }; 
-        if( mes > 0 && mes < 13 )
+        };
+
+        // verifica se mês é maior que zero e menor que 13
+        if (mes > 0 && mes < 13) // se verdade
+            // imprime o mês
             System.out.printf("Mês %d = %s ", mes, vetorMes[mes - 1]);
-        else
+        else // se falso
+             // imprime um aviso
             System.out.println("Digite um número entre 1 e 12 inclusive.");
     } // fim main
 } // fim classe
