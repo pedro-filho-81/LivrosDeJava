f41b14f7c543f8e3fb6d0aaf8c6a4c8822a3fc3d
PedroFilho <psfilho81@hotmail.com>
2022-04-12 09:38:53 -0300

    Update VerdadeiroFalse6.java

--- CienciaDaComputacao/Capitulo01/Exercicios1_4/Matriz/VerdadeiroFalse6.java
+++ CienciaDaComputacao/Capitulo01/Exercicios1_4/Matriz/VerdadeiroFalse6.java
@@ -2,55 +2,64 @@ package Matriz;
 
 public class VerdadeiroFalse6 {
     /**
-     * 1.4.6 Escreva um fragmento de código que imprima o conteúdo 
-     * de um array booleano bidimensional, usando * para representar 
-     * true e um espaço para representar false. Inclua índices de linha 
+     * 1.4.6 Escreva um fragmento de código que imprima o conteúdo
+     * de um array booleano bidimensional, usando * para representar
+     * true e um espaço para representar false. Inclua índices de linha
      * e coluna.
-     * Sedgewick, Robert; Wayne, Kevin. Ciência da Computação (pág. 118). 
+     * Sedgewick, Robert; Wayne, Kevin. Ciência da Computação (pág. 118).
      * Pearson Educação. Edição do Kindle.
      * Pedro, 11/04/2022
      */
     public static void main(String[] args) {
-        
+
         // declara, cria e inicializar uma matriz
         boolean[][] matriz = {
-                        {false, true, false},
-                        {true, false, true},
-                        {false, true, false},
-                        {false, true, false},
-                        {true, false, true},
-                        {false, true, false},
-                        {false, true, false},
-                        {true, false, true},
-                        {false, true, false},
-                        {false, true, false},
-                        {true, false, true},
-                        {false, true, false}                        
-                    };
-        // cabeçalhos
+                { false, true, false },
+                { true, false, true },
+                { false, true, false },
+                { false, true, false },
+                { true, false, true },
+                { false, true, false },
+                { false, true, false },
+                { true, false, true },
+                { false, true, false },
+                { false, true, false },
+                { true, false, true },
+                { false, true, false }
+        }; // false mostra #, true mostra *
+        
+        // cabeçalho
         System.out.println("Matriz original:");
-        System.out.print("              ");
-        System.out.printf("%s%8s%8s", "[1]", "[2]", "[3]" );
+        // espaços em branco para a coluna
+        System.out.print("                ");
+        // cabeçalho da primeira coluna
+        System.out.printf("%s", "1");
+        // loop para as outras colunas
+        for (int i = 1; i < 3; i++) {
+            // imprime o número da coluna
+            System.out.printf("%7d", i + 1);
+        } // fim for
+          // pula linha
         System.out.println();
 
         // loop for mpara mostra a matriz
-        for(int linha = 0; linha < 12; linha++ ){
-        
+        for (int linha = 0; linha < 12; linha++) {
+
             // imprima cabeçalho
             System.out.printf("linha - %2d", linha + 1);
-        
+
             // loop para coluna
-            for( int coluna = 0; coluna < 3; coluna++) {
-        
-                if(matriz[linha][coluna] == true)
+            for (int coluna = 0; coluna < 3; coluna++) {
+
+                if (matriz[linha][coluna] == true)
                     System.out.printf("%7s", "*");
                 else
                     System.out.printf("%7s", "#");
-        
-                } // fim for coluna
-        
+
+            } // fim for coluna
+
             System.out.println();
-        }// fim for linha
-    
+        } // fim for linha
+
     } // fim main
 } // fim classe
