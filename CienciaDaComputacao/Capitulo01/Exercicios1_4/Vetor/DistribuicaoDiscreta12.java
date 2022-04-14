8fb8164814fd5aa6a2af47e67984a14afb2c91e5
PedroFilho <psfilho81@hotmail.com>
2022-04-14 10:25:15 -0300

    Create DistribuicaoDiscreta12.java

---
+++ CienciaDaComputacao/Capitulo01/Exercicios1_4/Vetor/DistribuicaoDiscreta12.java
@@ -0,0 +1,39 @@
+package Vetor;
+
+public class DistribuicaoDiscreta12 {
+    public static void main(String[] args) {
+
+        // read in frequency of occurrence of n values
+        int n = args.length;
+        // vetor frequência
+        int[] freq = new int[n];
+        // loop para adicionar valor ao vetor
+        for (int i = 0; i < n; i++) {
+            // vetor recebe valores da linha de comando
+            // exp: java DistribuicaoDiscreta12.java 1 2 3 
+            freq[i] = Integer.parseInt(args[i]);
+        } // fim for
+
+        // compute total count of all frequencies
+        // soma o total dos elementos do vetor
+        int total = 0;
+        // loop para somar os valores do vetor
+        for (int i = 0; i < n; i++) {
+            // soma os elemntos do vetor
+            total += freq[i];
+        } // fim for
+
+        // generate random integer with probability proportional to frequency
+        // gera números aleatórios entre 0 e total
+        int r = (int) (total * Math.random());   // integer in [0, total)
+        int sum = 0;
+        int event = -1;
+        for (int i = 0; i < n && sum <= r; i++) {
+            sum += freq[i];
+            event = i;
+        }
+         
+        System.out.println(event);
+    }  
+    
+}
