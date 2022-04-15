12f9cededb63f81d5c8f3c64f451fc773ac1af9c
PedroFilho <psfilho81@hotmail.com>
2022-04-15 12:32:04 -0300

    Update SegundaTransposicao15.java

--- CienciaDaComputacao/Capitulo01/Exercicios1_4/Matriz/SegundaTransposicao15.java
+++ CienciaDaComputacao/Capitulo01/Exercicios1_4/Matriz/SegundaTransposicao15.java
@@ -3,14 +3,15 @@ package Matriz;
 import java.security.DomainLoadStoreParameter;
 
 public class SegundaTransposicao15 {
-    /** 1.4.15 Escreva um fragmento de código para transpor um array 
-        bidimensional quadrado sem criar um segundo array.
-        Sedgewick, Robert; Wayne, Kevin. Ciência da Computação (pág. 120). 
-        Pearson Educação. Edição do Kindle.
-        Pedro, 14/04/2022
-    */
+    /**
+     * 1.4.15 Escreva um fragmento de código para transpor um array
+     * bidimensional quadrado sem criar um segundo array.
+     * Sedgewick, Robert; Wayne, Kevin. Ciência da Computação (pág. 120).
+     * Pearson Educação. Edição do Kindle.
+     * Pedro, 14/04/2022
+     */
     public static void main(String[] args) {
-        
+
         // variável para entrada de dados via teclado
         int num = Integer.parseInt(args[0]);
 
@@ -19,9 +20,9 @@ public class SegundaTransposicao15 {
 
         // inicializa a magriz
         // loop para linha
-        for(int linha = 0; linha < num; linha++) {
+        for (int linha = 0; linha < num; linha++) {
             // loop para coluna
-            for(int coluna = 0; coluna < num; coluna++) {
+            for (int coluna = 0; coluna < num; coluna++) {
                 // inicializando a matriz
                 matriz[linha][coluna] = num * linha + coluna;
             } // fim for coluna
@@ -30,11 +31,11 @@ public class SegundaTransposicao15 {
         System.out.println("\nMatriz original:");
         mostrar(matriz);
 
-        // faz a transposição 
+        // faz a transposição
         // loop para linha
-        for(int linha = 0; linha < num; linha++) {
-            // loop para a coluna
-            for(int coluna = linha; coluna < num; coluna++) {
+        for (int linha = 0; linha < num; linha++) {
+            // loop para a coluna começando com o valor da linha
+            for (int coluna = linha; coluna < num; coluna++) {
                 // variável temporária
                 int temp = matriz[linha][coluna];
                 matriz[linha][coluna] = matriz[coluna][linha];
@@ -52,15 +53,15 @@ public class SegundaTransposicao15 {
     public static void mostrar(int[][] matriz) {
 
         // loop for para linha
-        for(int linha = 0; linha < matriz.length; linha++) {
+        for (int linha = 0; linha < matriz.length; linha++) {
             // loop para coluna
-            for(int coluna = 0; coluna < matriz.length; coluna++) {
+            for (int coluna = 0; coluna < matriz.length; coluna++) {
                 // mostra os elementos do vetor
                 System.out.printf("%5d", matriz[linha][coluna]);
             } // fim for coluna
             System.out.println();
         } // fim for linha
-    
+
     } // fim mostrar
 
 } // fim classe
