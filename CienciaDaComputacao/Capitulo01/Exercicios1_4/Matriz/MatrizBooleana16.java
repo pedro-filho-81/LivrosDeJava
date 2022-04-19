42e7a4955eba5949f3576d415f205b5d90548000
PedroFilho <psfilho81@hotmail.com>
2022-04-19 10:48:52 -0300

    Update MatrizBooleana16.java

--- CienciaDaComputacao/Capitulo01/Exercicios1_4/Matriz/MatrizBooleana16.java
+++ CienciaDaComputacao/Capitulo01/Exercicios1_4/Matriz/MatrizBooleana16.java
@@ -15,64 +15,82 @@ public class MatrizBooleana16 {
 
         // variável para argumento de linha de comando
         int num = Integer.parseInt(args[0]);
-        int dividendo = Integer.parseInt(args[1]);
-        int divisor = Integer.parseInt(args[2]);
+        int dividendo = 0;
+        int divisor = 0;
         int divisao = 1;
-        
+
         // declara e cria uma matriz boleana
-//        boolean[][] matriz = new boolean[num + 1][num + 1];
+        boolean[][] matriz = new boolean[num + 1][num + 1];
 
-        //for (int controle = 0; controle < 5; controle++) {
-/*
-            // Adiciona valores a matriz
-            // loop linha
-            for (int i = 1; i <= num; i++) {
-                // loop para a coluna
-                for (int j = 1; j <= num; j++) {
-                    matriz[i][j] = false;
-                } // fim for
+        // Adiciona valores a matriz
+        // loop linha
+        for (int i = 1; i <= num; i++) {
+            // loop para a coluna
+            for (int j = 1; j <= num; j++) {
+                matriz[i][j] = false;
             } // fim for
-*/
-                    
-        while (divisor != 0) {
-                    
-            System.out.println("dividendo = " + dividendo);
-            System.out.println("divisor = " + divisor);
+        } // fim for
 
-            divisao = dividendo / divisor;
-            System.out.printf("%d / %d = %d%n", dividendo, divisor, divisao);
-                  
-            int resto = dividendo % divisor;
-               
-            System.out.println("Resto = " + resto);
-               
-            dividendo = divisor;
-        
-            divisor = resto;
-               
-        } // fim while
+        System.out.println("Matriz original:");
+        // chama o método mostrar
+        mostrar(matriz);
 
-        // imprime
-        System.out.printf("Quando o resto for 0\ndividendo recebe o valor do divisor = %d%n", dividendo);
-        System.out.println("Que e o valor do MDC.");
-        // se número 1 igual a 1
-        if (dividendo == 1) {
-            System.out.printf("MDC = %3d Sao primos entre si.%n", dividendo);
-        } // fim if
-        else {
-            System.out.printf("MDC = %3d Nao sao primos entre si.%n", dividendo);
-        } // fim else
-        System.out.println();
-        
+        for (int i = 2; i <= num; i++) {
+            dividendo = i;
+            for (int j = 1; j <= num; j++) {
+                divisor = j;
+                while (divisor != 0) {
+
+                    //System.out.println("dividendo = " + dividendo);
+                    //System.out.println("divisor = " + divisor);
+
+                    divisao = dividendo / divisor;
+                    //System.out.printf("%d / %d = %d%n", dividendo, divisor, divisao);
+
+                    int resto = dividendo % divisor;
+
+                    //System.out.println("Resto = " + resto);
+
+                    dividendo = divisor;
+
+                    divisor = resto;
+
+                } // fim while
+
+                // imprime
+                //System.out.printf("Quando o resto for 0\ndividendo recebe o valor do divisor = %d%n", dividendo);
+                //System.out.println("Que e o valor do MDC = " + dividendo);
+                // se número 1 igual a 1
+                if (dividendo == 1) {
+                    //System.out.printf("Indica que %d e %d sao primos entre si.%n", i, j);
+                    matriz[i][j] = true;
+                } // fim if
+                else {
+                    //System.out.printf("Indica que %d e %d Nao sao primos entre si.%n", i, j);
+                    matriz[i][j] = false;
+                } // fim else
+
+                dividendo = i;
+
+//                System.out.println();
+
+            } // fim for j
+
+        } // fim for i
+
+        System.out.println("Matriz modificada");
+        // chama método mostrar
+        mostrar(matriz);
+
     } // fim main
 
     // método mostrar
     public static void mostrar(boolean[][] matriz) {
 
         // loop linha
-        for (int i = 2; i < matriz.length; i++) {
+        for (int i = 1; i < matriz.length; i++) {
             // loop para a coluna
-            for (int j = 2; j < matriz.length; j++) {
+            for (int j = 1; j < matriz.length; j++) {
                 System.out.printf("%8b", matriz[i][j]);
             } // fim for
             System.out.println();
