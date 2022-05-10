98066963d7a5a5b236dfe99a325987240170ba20
PedroFilho <psfilho81@hotmail.com>
2022-05-10 17:24:22 -0300

    Create FrequenciaDeVetor07.java

---
+++ ComoProgramar/Capitulo07/ExemplosCap07/FrequenciaDeVetor07.java
@@ -0,0 +1,30 @@
+public class FrequenciaDeVetor07 {
+    public static void main(String[] args) {
+
+        // vetor
+        int[] vetor = { 1, 2, 3, 3, 4, 5, 6, 6, 6, 5, 5, 2, 2, 3, 1,
+                1, 1, 6, 4, 2, 2, 3, 3, 1, 6, 6, 5, 4, 5, 4,
+                3, 2, 1, 4, 1, 12, 2, 3, 6, 5, 5, 4, 5, 6, 4,
+                2, 3, 1, 1, 6, 2 };
+
+        int[] frequencia = new int[7];
+
+        for (int resposta = 0; resposta < vetor.length; resposta++) {
+
+            try {
+                ++frequencia[vetor[resposta]];
+            } // fim try
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Indice da matriz fora dos limites de exceção");
+                System.out.printf("  vetor[%d} = %d%n%n", resposta, vetor[resposta]);
+            } // fim catch
+        } // fim for resposta
+
+        System.out.printf("%s%15s%n", "Indice", "Frequencia");
+
+        for (int mostrar = 1; mostrar < frequencia.length; mostrar++) {
+            System.out.printf("%3d%12d%n", mostrar, frequencia[mostrar]);
+        } // fim for mostrar
+
+    } // fim main
+} // fim classe
