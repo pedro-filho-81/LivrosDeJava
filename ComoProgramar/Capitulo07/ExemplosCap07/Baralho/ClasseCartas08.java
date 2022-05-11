4269030b7ca134e20bb55332ed9e18903f5aafca
PedroFilho <psfilho81@hotmail.com>
2022-05-11 12:04:26 -0300

    Create ClasseCartas08.java

---
+++ ComoProgramar/Capitulo07/ExemplosCap07/Baralho/ClasseCartas08.java
@@ -0,0 +1,16 @@
+package Baralho;
+public class ClasseCartas08 {
+    private final String faceDaCarta;
+    private final String naipeDaCarta;
+
+    // construtor
+    public ClasseCartas08(String faceDaCarta, String naipeDaCarta) {
+        this.faceDaCarta = faceDaCarta;
+        this.naipeDaCarta = naipeDaCarta;
+    } // fim construtor
+
+    
+    public static void main(String[] args) {
+        
+    } // fim main    
+} // fim classe
