d5feb2a58e3e1cb30044e454cdf8ca52e7e25f88
PedroFilho <psfilho81@hotmail.com>
2022-05-12 12:06:12 -0300

    Create TestaBaralhoDeCartas10.java

---
+++ ComoProgramar/Capitulo07/ExemplosCap07/Baralho/TestaBaralhoDeCartas10.java
@@ -0,0 +1,16 @@
+package Baralho;
+
+public class TestaBaralhoDeCartas10 {
+    public static void main(String[] args) {
+
+        BaralhoDeCartas09 meuBDeCartas09 = new BaralhoDeCartas09();
+
+        meuBDeCartas09.embaralhar();
+
+        for (int i = 1; i <= 52; i++) {
+            System.out.printf("%-19s", meuBDeCartas09.distribuiCartas());
+            if (i % 4 == 0)
+                System.out.println();
+        } // fim for
+    } // fim main
+} // fim classe
