234353569eb12a546fea299916252ece44f077f0
PedroFilho <psfilho81@hotmail.com>
2022-05-26 11:11:48 -0300

    Update ForAprimorado16.java

--- ComoProgramar/Capitulo07/TarefasCap07/ForAprimorado16.java
+++ ComoProgramar/Capitulo07/TarefasCap07/ForAprimorado16.java
@@ -10,6 +10,8 @@ public class ForAprimorado16 {
     public static void main(String[] args) {
 
         // variáveis double recebe argummento na linha de commando
+        // commpilação: javac ForAprimorado16.java 
+        // exp na linha de comando: java ForAprimorado16.java 1 2 3 4
         double n1 = Integer.parseInt(args[0]);
         double n2 = Integer.parseInt(args[1]);
         double n3 = Integer.parseInt(args[2]);
@@ -18,22 +20,25 @@ public class ForAprimorado16 {
         // vetor
         double[] vetor = { n1, n2, n3, n4 };
 
-        // calcular valores de n1 e n2
+        // calcula valores de n1 e n2
         double soma = 0;
 
         // loop for aprimmorado
         for (double valor : vetor)
+
             // soma os valores do vetor
             soma += valor;
 
         // cabeçalho
         System.out.println("\nVetor: ");
+
         // loop for aprimorado para mostrar valores do vetor
         for (double valor : vetor)
+
             // imprimme os valores do vetor
             System.out.printf("%7.2f", valor);
 
-        // immmprime a somma dos valores do vetor
+        // imprime a soma dos valores do vetor
         System.out.printf("\nSoma = %.2f\n", soma);
 
     } // fim main
