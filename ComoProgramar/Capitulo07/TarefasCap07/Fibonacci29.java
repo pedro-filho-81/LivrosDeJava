3fbee0b95c23ce05f58204d29f3405f24f1f7f76
PedroFilho <psfilho81@hotmail.com>
2022-07-06 17:22:25 -0300

    Update Fibonacci29.java

--- ComoProgramar/Capitulo07/TarefasCap07/Fibonacci29.java
+++ ComoProgramar/Capitulo07/TarefasCap07/Fibonacci29.java
@@ -4,13 +4,17 @@ public class Fibonacci29 {
 
     /**
      * 7.29 (Série de Fibonacci) A série de Fibonacci
-     * 0, 1, 1, 2, 3, 5, 8, 13, 21,inicia-se com os termos 0 e 1 e tem 
-     * a propriedade de que cada termo sucessivo é a soma dos dois termos precedentes.
-     * a) Escreva um método fibonacci(n) que calcule o enésimo número de Fibonacci. 
-     * Incorpore esse método a um aplicativo que permita ao usuário inserir o valor de n.
+     * 0, 1, 1, 2, 3, 5, 8, 13, 21,inicia-se com os termos 0 e 1 e tem
+     * a propriedade de que cada termo sucessivo é a soma dos dois termos
+     * precedentes.
+     * a) Escreva um método fibonacci(n) que calcule o enésimo número de Fibonacci.
+     * Incorpore esse método a um aplicativo que permita ao usuário inserir o valor
+     * de n.
      * b) Determine o maior número de Fibonacci que pode ser exibido em seu sistema.
-     * c) Modifique o aplicativo que você escreveu na parte (a) para utilizar double em vez 
-     * de int para calcular e retornar números de Fibonacci e utilizar esse aplicativo 
+     * c) Modifique o aplicativo que você escreveu na parte (a) para utilizar double
+     * em vez
+     * de int para calcular e retornar números de Fibonacci e utilizar esse
+     * aplicativo
      * modificado para repetir a parte (b)
      * Pedro, 03/07/2022
      */
@@ -23,7 +27,7 @@ public class Fibonacci29 {
         System.out.print("Digite o valor de n: ");
         int valor = input.nextInt();
         fibonacci(valor);
-        
+
     } // final principal
 
     // fibonacci
@@ -35,7 +39,7 @@ public class Fibonacci29 {
         int c = a + b;
 
         // loop para calcular fibonacci
-        while(a <= valor) {
+        while (a <= valor) {
 
             // exibir
             System.out.printf("%d\n", a);
@@ -44,6 +48,7 @@ public class Fibonacci29 {
             c = a + b;
 
         } // fi for
+
     } // final fibonacci
 
 } // final da classe
