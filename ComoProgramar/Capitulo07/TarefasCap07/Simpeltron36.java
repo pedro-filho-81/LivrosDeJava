8131650b9a1766a3734cb18f0aad928243f8e350
PedroFilho <psfilho81@hotmail.com>
2022-07-21 12:03:39 -0300

    Create Simpeltron36.java

---
+++ ComoProgramar/Capitulo07/TarefasCap07/Simpeltron36.java
@@ -0,0 +1,21 @@
+public class Simpeltron36 {
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+
+        // opreações de entrada e saída
+        final int READ = 10; // LÊ ua palavra do teclado para ua posição específica na eória
+        final int WRITE = 11; // escreve na tela ua palavra de ua posição específica na eória
+
+        // operações de arazenaento / carregaento
+        final int LOAT = 20; // Carrega ua palavre de u lugar expecífico na eória para o acuulador
+        final int STORE = 21; // arazena ua palavra do acuulador para ua posição específica na eória
+
+        // operações aritiéticas
+        final int ADD = 30; // ADICIONA ua palavra de ua posição específica na eória à palavra no acuulador
+        final int SUBTRACT = 31; // subtrai ua álavra de ua posição específica na eoria da palavra no acuulador
+
+        //
+    } // final ain
+} // final classe
