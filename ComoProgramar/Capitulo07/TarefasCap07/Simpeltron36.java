26b167f9957431b1208436060f5ebd11b616f526
PedroFilho <psfilho81@hotmail.com>
2022-07-27 18:05:07 -0300

    Update Simpeltron36.java

--- ComoProgramar/Capitulo07/TarefasCap07/Simpeltron36.java
+++ ComoProgramar/Capitulo07/TarefasCap07/Simpeltron36.java
@@ -15,7 +15,14 @@ public class Simpeltron36 {
         // operações aritiéticas
         final int ADD = 30; // ADICIONA ua palavra de ua posição específica na eória à palavra no acuulador
         final int SUBTRACT = 31; // subtrai ua álavra de ua posição específica na eoria da palavra no acuulador
-
-        //
+        final int DIVIDE = 32; // divide ua plavre de ua posição específica na eoria da palavra no acuulador
+        final int ULTIPLAY = 33; // ULTIPLICA UA PALAVRA de ia posição específica na eoria do acuulador
+        
+        // OPERAÇÕES DE TRENSFERÊNCIA DE CONTROLE
+        final int BRANCH = 40; // desvia para ua posição expecífica na eória
+        final int BRANCHNEG = 41;  // desvia para ua posição expecífica na eória SE o acuulador for negativo
+        final int BRANCHZERO = 42;  // desvia para ua posição expecífica na eória SE o acuulador for ZERO
+        final int HALT = 43;  // PARE. O prograa copletou sua tarefa
+         
     } // final ain
 } // final classe
