5ad9d70f6848713ec0d1f48f6deb641f3746d5f2
PedroFilho <psfilho81@hotmail.com>
2022-07-28 16:44:19 -0300

    Update Time1.java

--- ComoProgramar/Capitulo08/ExemplosCap08/Time1/Time1.java
+++ ComoProgramar/Capitulo08/ExemplosCap08/Time1/Time1.java
@@ -1,38 +1,36 @@
-
-// Timi1.java
-// Cria a classe Time1 mantém a data/hora no formato de 24 horas
 public class Time1 {
-    
-    // variáveis de instancia
-    private int horas; // 0 / 23
-    private int minutos; // 0 / 59
-    private int segundos; // 0 / 59
+    /**
+     * mantem a data/hora no forato de 24 horas
+     * @throws IllegalAccessException
+     */
 
-    // configura um novo valor de tempo usando hora universal;
-    // lança umma exceção se ahora, minutos ou segundo for inválido.
-    public void setTime( int horas, int minutos, int segundos) 
-    {
-        if(horas < 0 || horas >= 24 || minutos < 0 || minutos >= 60 || segundos < 0 || segundos >= 60)
-        {
-            throw new IllegalArgumentException("Horas, minutos e/ou segundos fora do intervalo adequado.");
-        } // fimm if throw
+    // variáveis de instância
+    private int horas;
+    private int minutos;
+    private int segundos;
+
+    public void setTime(int horas, int minutos, int segundos) 
+                        throws IllegalAccessException {
+        // validando horas, minutos e segundos
+        if(horas < 0 || horas >= 24 || minutos < 0 || minutos >= 60 ||
+            segundos < 0 || segundos >= 60 ) {
+                throw new IllegalAccessException("Horas, minutos e/ou segundos fora do padrao ideal");
+        } // final if
 
         this.horas = horas;
         this.minutos = minutos;
         this.segundos = segundos;
-    } // fim setTime
 
-    // converte em String no formado data/hora universal (HH:MM:SS)
-    public String toUniversalString()
-    {
+    } // final setTime
+
+    // converte em String no fomrato de data/hora universal (HH:mm:SS)
+    public String toUniversalString() {
         return String.format("%02d:%02d:%02d", horas, minutos, segundos);
-    } // fim toUniversalString
+    } // final toUniversalString
 
-    //converte em String no formato data/hora padrão (HH:MM:SS AM/PM)
-    public String toString()
-    {
-        return String.format("%d:%02d:%02d %s", (horas == 0 || horas == 12 ? 12 : horas % 12),
-                                                        minutos, segundos, (horas < 12 ? "AM" : "PM") );          
-    } // FIM toString
-
-} // fim classe
+    // converte e String no forato padrão de data/hora (H:mm:SS Am OU Pm)
+    public String toString() {
+        return String.format( "%d:%02d:%02d %s", ((horas == 0 || horas == 12) ? 12 : horas % 12), 
+                                minutos, segundos, (horas < 12 ? "Am" : "Pm"));
+    } // final toString
+} // final classe
