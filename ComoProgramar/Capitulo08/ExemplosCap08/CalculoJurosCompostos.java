9c5fd71fee2dfac3c82c9e15e1b56bd5a7d52e8f
PedroFilho <psfilho81@hotmail.com>
2022-08-08 11:50:20 -0300

    Create CalculoJurosCompostos.java

---
+++ ComoProgramar/Capitulo08/ExemplosCap08/CalculoJurosCompostos.java
@@ -0,0 +1,31 @@
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+/**
+ * Interest.java
+ * Cálculos de juros compostos com BigDecimal.
+ */
+
+ public class CalculoJurosCompostos {
+    public static void main(String[] args)
+    {
+        //quantidade principal antes dos juros
+        BigDecimal principal = BigDecimal.valueOf(1000.0); // R$ 1000,0
+        // taxa de juros anual 5% ou 0,05 ou 5/100
+        BigDecimal taxa = BigDecimal.valueOf(0.05); // 5%
+
+        // exibe cabeçalho
+        System.out.printf("%s%20s%n", "Ano", "Saldo/Déposito");
+
+        // loop para exibir o saldo
+        for(int ano = 1; ano <= 10; ano++)
+        {
+            // calcula os novos valores com a taxa de juros anual
+            BigDecimal saldo = principal.multiply(taxa.add(BigDecimal.ONE).pow(ano));
+        
+            System.out.printf("%4d%20s%n", ano, 
+                            NumberFormat.getCurrencyInstance().format(saldo));
+        } // final for
+
+    } // final main
+} // final classe
