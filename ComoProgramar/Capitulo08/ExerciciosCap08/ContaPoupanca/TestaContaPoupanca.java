050ae8b24047e1490fa3e1a4557299c40b433b7c
PedroFilho <psfilho81@hotmail.com>
2022-08-21 17:40:58 -0300

    Update TestaContaPoupanca.java

--- ComoProgramar/Capitulo08/ExerciciosCap08/ContaPoupanca/TestaContaPoupanca.java
+++ ComoProgramar/Capitulo08/ExerciciosCap08/ContaPoupanca/TestaContaPoupanca.java
@@ -1,29 +1,40 @@
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class TestaContaPoupanca {
-    public static void main(String[] args)
+    public static void main(String[] args) 
     {
         // entrada de dados iniciais
-        BigDecimal principal = BigDecimal.valueOf(1000.0);
-        BigDecimal taxa = BigDecimal.valueOf(0.05);
-
-        ContaPoupanca8_6 poupanca = new ContaPoupanca8_6(100, principal, taxa, 1);
-
-        System.out.println("\nConta Poupanca:");
-        System.out.printf("Conta: %d\nCapital R$ %.2f\nTaxa: %.2f%%\nSaldo = %.2f\n", poupanca.getNumeroDaConta(),
-                                poupanca.getValorPrincipal(), poupanca.getTaxaDeJurosAnual(),
-                                poupanca.getSaldo());
-
-        // depósitos, taxa e tempo
-        BigDecimal deposito = BigDecimal.valueOf(1000);
+        BigDecimal principal1 = BigDecimal.valueOf(2000.0); // valor inicial
+        BigDecimal principal2 = BigDecimal.valueOf(3000.0); // valor inicial
+        BigDecimal taxa = BigDecimal.valueOf(0.05); // taxa de juros inicial
         BigDecimal novaTaxa = BigDecimal.valueOf(0.03);
-        int tempo = 2;
-        poupanca.setTaxaDeJurosAnual(novaTaxa);
-        poupanca.calculaJurosCompostos(deposito, taxa, tempo);
-        System.out.println("\nConta Poupanca nova taxa:");
-        System.out.printf("Conta: %d\nCapital R$ %.2f\nTaxa: %.2f%%\nTempo: %d%nSaldo = %.2f\n", poupanca.getNumeroDaConta(),
-                                poupanca.getValorPrincipal(), poupanca.getTaxaDeJurosAnual(),
-                                tempo ,poupanca.getSaldo());
+        BigDecimal saldoAtual;
+
+        // cria objeto poupança
+        ContaPoupanca8_6 poupanca1 = new ContaPoupanca8_6(100, principal1, taxa, 1);
+        poupanca1.calculaJurosCompostos(principal1, taxa, 1);      
+        exibirConta(poupanca1);
+        saldoAtual = poupanca1.getSaldo();
+        poupanca1.modificarTaxaDeJuros(novaTaxa, saldoAtual, 1);
+        exibirConta(poupanca1);
+        
+        ContaPoupanca8_6 poupanca2 = new ContaPoupanca8_6(200, principal2, taxa, 1);
+        poupanca2.calculaJurosCompostos(principal2, taxa, 1);
+        exibirConta(poupanca2);
+        saldoAtual = poupanca2.getSaldo();
+        poupanca2.modificarTaxaDeJuros(novaTaxa, saldoAtual, 1);
+        exibirConta(poupanca2);
 
     } // final main
+
+    public static void exibirConta(ContaPoupanca8_6 conta)
+    {
+        // exibe
+        System.out.println("\nConta Poupanca:");
+        System.out.printf("Conta: %d\nCapital R$ %.2f\nTaxa: %.2f%%\nSaldo = %.2f\n", conta.getNumeroDaConta(),
+                conta.getValorPrincipal(), conta.getTaxaDeJurosAnual(),
+                conta.getSaldo());
+    } // final exibir conta
+
 } // final classe
