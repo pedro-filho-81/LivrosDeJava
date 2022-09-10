3d783b4c40fc9f31f080585535f68d2d878de260
PedroFilho <psfilho81@hotmail.com>
2022-09-10 12:08:37 -0300

    Create BaralhoDeCartas02.java

---
+++ ComoProgramar/Capitulo07/TarefasCap07/BaralhoDeCartas02/BaralhoDeCartas02.java
@@ -0,0 +1,13 @@
+import java.security.SecureRandom;
+
+public class BaralhoDeCartas02 {
+
+    private Cartas01[] baralho;
+    private int cartaAtual;
+    private static final int NUERO_DE_CARTAS = 52;
+
+    // GERADOR aleatorio
+    private static final SecureRandom aleatorio = new SecureRandom();
+
+    
+} // final classe
