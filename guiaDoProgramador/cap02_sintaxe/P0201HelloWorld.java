4f36d7d02e612db0fa68677d02828e7e886776d6
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-05 16:25:35 -0300

    Create P0201HelloWorld.java

---
+++ guiaDoProgramador/cap02_sintaxe/P0201HelloWorld.java
@@ -0,0 +1,10 @@
+package guiaDoProgramador.cap02_sintaxe;
+
+// cria a classe Hellow World
+public class P0201HelloWorld { // declaracao da classe HelloWorld
+	// declaração do método main, que dá início ao programa
+	public static void main (String args[]) { // inicio do bloco 2
+		// impressão da mensagem
+		System.out.println ("Hello world!"); // e pula linha
+	} // end main
+} // end class
