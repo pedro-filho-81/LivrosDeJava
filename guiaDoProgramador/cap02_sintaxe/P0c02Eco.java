eb7762caa6f875578bb499815c273dfe1698db05
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-05 16:25:58 -0300

    Create P0c02Eco.java

---
+++ guiaDoProgramador/cap02_sintaxe/P0c02Eco.java
@@ -0,0 +1,13 @@
+package guiaDoProgramador.cap02_sintaxe;
+// cria a classe Eco
+public class P0c02Eco { // inicio da classe
+   // cria o método main
+   public static void main(String args[]) { // inicio do bloco 2
+      int i;
+      // loop para imprimir 10 números
+      for(i = 0; i < 10; i++) { // inicio loop for
+      // imprime na tela
+         System.out.println(args[i]); 
+      } // end for      
+   } // end main
+} // end class
