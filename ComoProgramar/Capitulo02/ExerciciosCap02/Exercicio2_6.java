f4a45afcce63c137dd4b6108fe368458e1a0dfa1
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-06 17:00:31 -0300

    Update Exercicio2_6.java

--- ComoProgramar/Capitulo02/ExerciciosCap02/Exercicio2_6.java
+++ ComoProgramar/Capitulo02/ExerciciosCap02/Exercicio2_6.java
@@ -3,14 +3,12 @@
 import java.util.Scanner; // programa utiliza Scanner
   
 public class Exercicio2_6 {
-  public static void main(String[] args)
-  {
+  public static void main(String[] args)  {
     // cria Scanner para obter entrada a partir da janela de comando
     Scanner input = new Scanner(System.in);
 
     int x; // primeiro número inserido pelo usuário
     int y; // segundo número inserido pelo usuário
-    }
     int z; // terceiro número inserido pelo usuário
     int result; // produto dos números
 
@@ -26,5 +24,5 @@ public class Exercicio2_6 {
     result = x * y * z; // calcula o produto dos números
 
     System.out.printf("Product is %d%n", result);
-  } // fim do método main
-} // end class 
+  } // fim main
+} // end class
