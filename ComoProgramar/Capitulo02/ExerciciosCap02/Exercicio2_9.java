458e1d206d068b580e9c9efe365e8e62e9d0b5db
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-06 17:01:02 -0300

    Create Exercicio2_9.java

---
+++ ComoProgramar/Capitulo02/ExerciciosCap02/Exercicio2_9.java
@@ -0,0 +1,23 @@
+/*
+ * 2.9 Determine se cada uma das seguintes afirmações é verdadeira ou falsa. Se falsa, explique por quê.
+ * a) Operadores Java são avaliados da esquerda para a direita.
+ * b) Os seguintes nomes são todos de variável válidos: _under_bar_, m928134, t5, j7,
+ * her_sales$, his_$account_total, a, b$,c, z e z2.
+ * c) Uma expressão aritmética Java válida sem parênteses é avaliada da esquerda para a direita.
+ * d) Os seguintes nomes são todos de variável inválidos: 3g, 87, 67h2, h22 e 2h
+ */
+public class Exercicio2_9 {
+   public static void main(String[] args) {
+      
+      // a) Operadores Java são avaliados da esquerda para a direita.
+         /* verdade SE NÃO TIVER PARÊNTESE () */
+      // b) Os seguintes nomes são todos de variável válidos: _under_bar_, m928134, t5, j7,
+      // her_sales$, his_$account_total, a, b$,c, z e z2.
+         /* verdadeiro */
+      // c) Uma expressão aritmética Java válida sem parênteses é avaliada da esquerda para a direita.
+         /* vERDADEIRO */
+      // d) Os seguintes nomes são todos de variável inválidos: 3g, 87, 67h2, h22 e 2h
+         /* FALSO h22 É UmA VARIÁVEL VÁLIDA */
+
+   } // end main
+} // end class
