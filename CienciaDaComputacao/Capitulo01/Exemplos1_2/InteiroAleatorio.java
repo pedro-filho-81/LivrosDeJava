b9ad298583311ac28dfce4acfa55095b5aa817f3
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-07 17:21:28 -0300

    Update InteiroAleatorio.java

--- CienciaDaComputacao/Capitulo01/Exemplos1_2/InteiroAleatorio.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_2/InteiroAleatorio.java
@@ -1,11 +1,19 @@
 package Exemplos1_2;
-
-
+/*
+ * Este programa usa o método Java Math.random() para gerar um número 
+ * aleatório r entre 0,0 (inclusive) e 1,0 (exclusive); então multiplica 
+ * r pelo argumento de linha de comando n para obter um número aleatório 
+ * maior ou igual a 0 e menor que n; então usa uma conversão para truncar 
+ * o resultado para ser um valor inteiro entre 0 e n-1.
+ * Sedgewick, Robert; Wayne, Kevin. Computer Science: 
+ * An Interdisciplinary Approach (p. 34). Pearson Education. 
+ * Edição do Kindle.
+ */
 public class InteiroAleatorio {
     public static void main(String[] args) {
 
         // variável recebe valor do usuário
-        int n = 5;
+        int n = 1000;
 
         // gera número aleatório entre 0.0 e 1.0
         double r = Math.random();
