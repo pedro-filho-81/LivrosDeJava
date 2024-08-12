4276b9250dbe2afa2dac1084f33d16cab3680224
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-12 12:08:28 -0300

    Create ArrayInregulares06.java

---
+++ javaParaIniciantes/capitulo05/exemplos/ArrayInregulares06.java
@@ -0,0 +1,30 @@
+package javaParaIniciantes.capitulo05.exemplos;
+
+// Aloca manualmente segundas dimensões de tamanhos diferentes
+
+public class ArrayInregulares06 {
+   public static void main(String[] args) {
+      
+      // array matriz
+      int matriz[][] = new int[7][];
+
+      // segundas dimensões para 10 elementos
+      // colunas para 10 elementos
+      matriz[0] = new int[10];
+      matriz[1] = new int[10];
+      matriz[2] = new int[10];
+      matriz[3] = new int[10];
+      matriz[4] = new int[10];
+
+      // coluna de 2 elementos
+      matriz[5] = new int[2];
+      matriz[6] = new int[2];
+
+      // variáveis
+       int i, j;
+
+
+
+
+   } // end main
+} // end class
