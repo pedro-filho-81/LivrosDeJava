d4aab3560c852749befc030decfa1616333a8577
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-21 17:38:12 -0300

    Update InicializandoVetor02.java

--- CienciaDaComputacao/Capitulo01/Exemplos1_4/InicializandoVetor02.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_4/InicializandoVetor02.java
@@ -3,19 +3,25 @@ package Exemplos1_4;
 public class InicializandoVetor02 {
     public static void main(String[] args) {
 
-        // declarando e inicializando vetor
-        // tipo-nome= inicializa [10] quantidade de elementos
-        // tipo double, nome x, novo double[tamanho => 10] 
+        /*  Declarando e inicializando vetores
+            double é o tipo do vetor, 
+            x e y são os nomes do vetor
+            a palavra-chave new torna as 
+            variáveis um objeto double
+            o valor 10 é a quantidade de
+            elementos que os vetores comportam.*/ 
         double[] x = new double[10];
         double[] y = new double[10];
 
         // criando variáveis
         double soma = 0.0;
 
-        // adicionando valores aos vetores x e y
+        // loop for para adicionar valores aos vetores x e y
         for (int i = 0; i < 10; i++) {
-            x[i] = Math.random() * 30; // adiciona valores a x
-            y[i] = Math.random() * 20; // adiciona valores a y
+            // vetor x recebe valores aleatóris
+            x[i] = Math.random() * 30;
+            // vetor y também recebe valores aleatórios
+            y[i] = Math.random() * 20;
         } // fim for
 
         // cabeçalho
@@ -27,8 +33,12 @@ public class InicializandoVetor02 {
             soma += x[i] * y[i]; // soma os produtos de x e y
 
             // depois imprimir o resultado
-            System.out.println("\t " + (int) x[i] + "\t " + (int) y[i] + "\t   " +
-                                 (int) x[i] * (int) y[i] + "\t         " + (int) soma);
+            System.out.println("\t " + 
+            (int) x[i] + "\t " + 
+            (int) y[i] + "\t   " + 
+            (int) x[i] * 
+            (int) y[i] + "\t         " + 
+            (int) soma);
         } // fim for
     } // fim main
 }// fim classe
