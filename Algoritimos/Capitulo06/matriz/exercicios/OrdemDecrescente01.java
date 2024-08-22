0ec73f90e2987f5c6221b4ef181d0be23828fc1b
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-22 17:22:29 -0300

    Create OrdemDecrescente01.java

---
+++ Algoritimos/Capitulo06/matriz/exercicios/OrdemDecrescente01.java
@@ -0,0 +1,29 @@
+package Algoritimos.Capitulo06.matriz.exercicios;
+/*
+   MMMM
+    * Elaborar um programa que leia dez elementos numéricos inteiros em uma 
+    matriz A de uma dimensão do tipo vetor. Construir uma matriz B de mesmos 
+    tipo e dimensão com os elementos da matriz A divididos por cinco. 
+    Apresentar os elementos armazenados na matriz B na ordem decrescente.
+    JOSÉ AUGUSTO N. G. MANZANO, JAYR FIGUEIREDO DE OLIVEIRA. Algoritmos (Portuguese Edition) (p. 361). Editora Saraiva. Edição do Kindle. 
+    */
+
+import java.security.SecureRandom;
+
+public class OrdemDecrescente01 {
+   
+   public static void main(String[] args) {
+      // objeto
+      SecureRandom alea = new SecureRandom();
+
+      // constante
+      final int SIZE = 10;
+
+      // variáveis
+      int i, j;
+      int[] vetorA = new int[SIZE];
+      double[] vetorB = new double[SIZE];
+      double x;
+
+   } // end main
+} // end class
