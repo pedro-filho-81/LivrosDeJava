0ef18add864b530198a1deb8434fb99c0aca8487
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-22 17:23:26 -0300

    Create teste.java

---
+++ Algoritimos/Capitulo07/arrays/exercicios/teste.java
@@ -0,0 +1,16 @@
+package Algoritimos.Capitulo07.arrays.exercicios;
+
+public class teste {
+   public static void main(String[] args) {
+      
+   int a = 5;
+   System.out.println("a = " + a);
+   int b = 2;
+   System.out.println("b = " + b);
+   int c = a + b; 
+   System.out.println("c = a + b => " + c);
+
+   System.out.println("\nO valor de c é: " + c);
+
+   }
+}
