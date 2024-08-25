b3e29a2637f3b212a9b4a520575f86f2a5de8097
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-25 18:12:23 -0300

    Create VetorAParesImpares23.java

---
+++ Algoritimos/Capitulo06/Vetor/ExerFixacao/VetorAParesImpares23.java
@@ -0,0 +1,95 @@
+import java.security.SecureRandom;
+/**
+ * v)Construir um programa que leia uma matriz A de 
+ * uma dimensão do tipo vetor com 30 elementos do 
+ * tipo inteiro. Ao final do programa, apresentar a 
+ * quantidade de valores pares e ímpares existentes 
+ * na referida matriz.
+ * JOSÉ AUGUSTO N. G. MANZANO, JAYR FIGUEIREDO DE 
+ * OLIVEIRA. Algoritmos (Portuguese Edition) 
+ * (p. 315). Editora Saraiva. Edição do Kindle. 
+ * Pedro, 25/08/2024
+ */
+public class VetorAParesImpares23 {
+      public static void main(String[] args) {
+      
+      // constante índice
+      final int SIZE = 30;
+
+      // vetores
+      int[] vetorA = new int[SIZE];
+
+      // resultado
+      System.out.println("VetorA:");
+      adicionar(vetorA);
+      exibirVetor(vetorA);
+
+   } // end main
+
+   // método adicionar valores pares
+   public static void adicionar(int[] vetor) {
+
+      // objeto
+      SecureRandom alea = new SecureRandom();
+
+      // variáveis
+      int i = 0, j = 0;
+      int item = 0;
+
+      do {
+         // adiciona valor aleatório
+         int valorAlea = 1 + alea.nextInt(100);
+         // indica que o vetor tem um valor igual
+         item = 1; // e o incremento não será executado
+
+         // vetor recebe o valor
+         vetor[i] = valorAlea;
+         // e informa que o contador pode ser incrementado
+         item = 0; //  reinicializa
+         
+         // loop pelo vetor para verificar se 
+         // o vetor tem um valor igual
+         for(j = 0; j < i; j++) {
+            // se os vetores tiverem valores iguais
+            if (vetor[j] == vetor[i]) {
+               // informa que exixte no vetor um valor igual
+               item = 1;
+            } // end if
+         } // end for
+
+         // se o valor adicionado ao vetor não for igual
+         if (item == 0) {
+            // incrementa i em 1
+            i++;
+
+         } // end if item
+
+         // faça enquanto o índice for menor que o vetor
+      } while (i < vetor.length);
+   } // end adicionar
+   
+    // método exibir
+    public static void exibirVetor(int[] vetor) {
+      // variaveis
+      int somaPar = 0;
+      int somaImpar = 0;
+
+      System.out.print("{");
+      // loop para exibir vetor
+      for (int i = 0; i < vetor.length; i++) {
+          // exibir valores
+          System.out.printf("%5d", vetor[i]);
+          if (vetor[i] % 2 == 0) {
+            somaPar++;
+          } else {
+            somaImpar++;
+          } // end else
+      } // fi for
+        // pula linha
+      System.out.println("  };");
+      
+      System.out.printf("O vetor tem %d valores pares.\n", somaPar);
+      System.out.printf("O vetor tem %d valores ímpares.", somaImpar);
+  } // end exibir
+
+} // end class
