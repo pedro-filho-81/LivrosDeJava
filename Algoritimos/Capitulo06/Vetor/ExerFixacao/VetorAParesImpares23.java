94527825cb84ab94e91b1755323c158eb0de9003
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-27 10:39:05 -0300

    Update VetorAParesImpares23.java

--- Algoritimos/Capitulo06/Vetor/ExerFixacao/VetorAParesImpares23.java
+++ Algoritimos/Capitulo06/Vetor/ExerFixacao/VetorAParesImpares23.java
@@ -26,7 +26,7 @@ public class VetorAParesImpares23 {
 
    } // end main
 
-   // método adicionar valores pares
+   // método adicionar valores não repetidos ao vetor
    public static void adicionar(int[] vetor) {
 
       // objeto
@@ -37,39 +37,42 @@ public class VetorAParesImpares23 {
       int item = 0;
 
       do {
-         // adiciona valor aleatório
+         // adiciona valor suposto aleatório a variável
          int valorAlea = 1 + alea.nextInt(100);
          // indica que o vetor tem um valor igual
-         item = 1; // e o incremento não será executado
+         item = 1; // e o incremento não será executado (loop infinito)
 
-         // vetor recebe o valor
+         // vetor recebe o valor aleatório;
          vetor[i] = valorAlea;
          // e informa que o contador pode ser incrementado
          item = 0; //  reinicializa
          
          // loop pelo vetor para verificar se 
-         // o vetor tem um valor igual
+         // o vetor tem um valor igual ao informado acima
          for(j = 0; j < i; j++) {
             // se os vetores tiverem valores iguais
             if (vetor[j] == vetor[i]) {
-               // informa que exixte no vetor um valor igual
-               item = 1;
+               // informa que existe no vetor um valor igual
+               item = 1; // e o loop se repete sem fazer a incrementação
             } // end if
          } // end for
 
          // se o valor adicionado ao vetor não for igual
+         // ou seja se item igual a zero
          if (item == 0) {
             // incrementa i em 1
             i++;
-
          } // end if item
 
-         // faça enquanto o índice for menor que o vetor
+         // faça enquanto o índice i for menor que o tamanho vetor
+         // ou seja, se verdade, repete tudo outra vez, 
+         // até a condição ser falsa.
       } while (i < vetor.length);
    } // end adicionar
    
     // método exibir
     public static void exibirVetor(int[] vetor) {
+      
       // variaveis
       int somaPar = 0;
       int somaImpar = 0;
@@ -78,17 +81,23 @@ public class VetorAParesImpares23 {
       // loop para exibir vetor
       for (int i = 0; i < vetor.length; i++) {
           // exibir valores
-          System.out.printf("%5d", vetor[i]);
-          if (vetor[i] % 2 == 0) {
+          System.out.printf("%3d", vetor[i]);
+
+          // verifica se o valor do vetor é par ou ímpar
+          if (vetor[i] % 2 == 0) { // se par
+            // soma 1 como valor par
             somaPar++;
-          } else {
+          } else { // se não
+            // soma 1 como valor ímpar
             somaImpar++;
           } // end else
-      } // fi for
+      } // fim for
         // pula linha
       System.out.println("  };");
       
+      // exibe o resultado dos valores pares
       System.out.printf("O vetor tem %d valores pares.\n", somaPar);
+      // resultado dos valores ímpares
       System.out.printf("O vetor tem %d valores ímpares.", somaImpar);
   } // end exibir
 
