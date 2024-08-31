4ddebb2460d36bbfa29b2b2e6d2efd02299c44b5
PedroSantosFilho <psfilho81@hotmail.com>
2024-08-31 16:28:18 -0300

    Update Exercicio4_4.java

--- ComoProgramar/Capitulo04/ExerciciosCap04/Exercicio4_4/src/exercicio4_4/Exercicio4_4.java
+++ ComoProgramar/Capitulo04/ExerciciosCap04/Exercicio4_4/src/exercicio4_4/Exercicio4_4.java
@@ -1,9 +1,9 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exercicio4_4;
 
 /**
  *  4.4 Escreva instruções Java para realizar cada uma das seguintes tarefas:
