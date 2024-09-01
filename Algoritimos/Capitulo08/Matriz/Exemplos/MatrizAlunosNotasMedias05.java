d75a13c25b97157c963ef985e99e2ce614fabb0c
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-01 12:15:31 -0300

    Create MatrizAlunosNotasMedias05.java

---
+++ Algoritimos/Capitulo08/Matriz/Exemplos/MatrizAlunosNotasMedias05.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MatrizAlunosNotasMedias05 {
+   /**
+    * Desenvolver um programa que leia os nomes de oito 
+    alunos e também suas quatro notas bimestrais. Ao final, 
+    deve apresentar o nome de cada aluno classificado em 
+    ordem alfabética, bem como suas médias e a média geral 
+    dos oito alunos. 
+    JOSÉ AUGUSTO N. G. MANZANO, JAYR FIGUEIREDO DE OLIVEIRA. 
+    Algoritmos (Portuguese Edition) (p. 397). 
+    Editora Saraiva. Edição do Kindle. 
+    01/09/2024
+    */
+   public static void main(String[] args) {
+      
+   } // end Main
+
+   // adiciona nomes
+   public static void adicionarNomes(String[] nomes) {
+
+   } // end adicionar nomes
+
+   // adicionar médias
+   public static void adicionarNotas(double[][] matriz, int linha, int coluna) {
+
+   } // end adicionar notas
+
+   // adicionar médias
+   public static void adicionarMedia(double[] vetor) {
+
+   } // end adicionar média
+
+} // end class 
