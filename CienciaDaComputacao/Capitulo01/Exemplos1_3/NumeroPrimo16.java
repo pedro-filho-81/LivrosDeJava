eecaf1950fe47f14ea98325793dde4ee73125620
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-12 19:30:53 -0300

    Update NumeroPrimo16.java

--- CienciaDaComputacao/Capitulo01/Exemplos1_3/NumeroPrimo16.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/NumeroPrimo16.java
@@ -12,7 +12,7 @@ public class NumeroPrimo16 {
         // entrada de dados
         System.out.print("Informe um valor para saber se é primo: ");
         long n = input.nextLong();
-
+        System.out.println("n = " + n);
         // variáveis
         boolean isPrime = true;
 
@@ -23,14 +23,21 @@ public class NumeroPrimo16 {
 
         // tente todos os fatores possíveis de n
         // se n tem um fator, então tem um menor ou igual a sqrt (n),
-        // então, para eficiência, só precisamos verificar o fator <= sqrt (n) ou
+        // então, para eficiência, só precisamos verificar se o fator <= sqrt (n) ou
         // fator equivalente * fator <= n
         for (long factor = 2; factor * factor <= n; factor++) {
-
-            // se o número após divisão pelo fator tiver o resto igual a zero
+            
+            System.out.println("factor no for = " + factor);
+            System.out.println(n + " % " + factor + " == " + n % factor);
+            
+    
+            // se o número após divisão pelo fator 
+            // tiver o resto igual a zero não é primo
             if (n % factor == 0) {
                 // valor não é primo
-                isPrime = false;
+                 isPrime = false;
+                System.out.println("if(" + n + " % " + factor + " == " + 0 + ")");
+                System.out.println("isPrime = " + isPrime);
                 break; // saia do programa
             } // fim if
         } // fim for
@@ -42,6 +49,8 @@ public class NumeroPrimo16 {
         else // se não
              // imprima não é primo
             System.out.println(n + " não é primo");
+        
+        input.close();
 
     } // fim main
 } // fim classe
