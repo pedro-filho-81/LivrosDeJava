2eed3d003253402c53559f260a14913111fb7b64
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-12 11:30:22 -0300

    Update AnoBissexto06.java

--- CienciaDaComputacao/Capitulo01/Exemplos1_3/AnoBissexto06.java
+++ CienciaDaComputacao/Capitulo01/Exemplos1_3/AnoBissexto06.java
@@ -5,23 +5,31 @@ import java.util.Scanner;
 public class AnoBissexto06 { 
     public static void main(String[] args) { 
 
-        try (// cria e inicializa o objeto input da classe Scanner
-        Scanner input = new Scanner(System.in)) {
-            // entrada de dados
+        try (
+        
+            // cria e inicializa o objeto input da classe Scanner
+            Scanner input = new Scanner(System.in)) {
+            
+           // entrada de dados
             System.out.print( "Digite um ano: ");
             int ano = input.nextInt();
             boolean anoBissexto;
 
-            // divisíval por 4
-            anoBissexto = (ano % 4 == 0);
+            // verifica se ano é divisíval por 4
+            anoBissexto = (ano % 4 == 0); // tem que ser true
 
-            // divisível por 4 e não por 100
+            // se ano divisível por 4 (true)
+            // e não divisível por 100 (true)
+            // aqui tem que ser true também
             anoBissexto = anoBissexto && (ano % 100 != 0);
 
-            // divisível por 4 e não por 100 e divisível por 400
-            anoBissexto = anoBissexto || (ano % 400 == 0);
+            // verifica se ano é divisível por 4 (true)
+            // e não divisível por 100 (true)
+            // e divisível por 400 (true) também
+            // se algum acima ou ambos forem (false)
+            anoBissexto = anoBissexto || (ano % 400 == 0); // aqui um ou ambos tem que ser (true)
 
-            // imprime resultado
+            // imprime resultado true
             System.out.println( ano + " é um ano bissexto? " + anoBissexto );
         }
 
