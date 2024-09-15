2f5cbf531c790f7c18c935e856aae1b1269291d6
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-15 19:13:34 -0300

    Create Fibonacci09.java

---
+++ Algoritimos/cap05_loops/exercicios/Fibonacci09.java
@@ -0,0 +1,61 @@
+package cap05_loops.exercicios;
+/**
+ * i)Escrever um programa que apresente os valores da 
+ * sequência numérica de Fibonacci até o décimo quinto 
+ * termo. A sequência de Fibonacci é formada pelos 
+ * valores numéricos 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 
+ * 55, 89, 144, 233, 377, ... etc., obtendo-se o 
+ * próximo termo a partir da soma do termo atual com o 
+ * anterior sucessivamente até o infinito se a 
+ * sequência não for interrompida, sendo determinada a 
+ * partir da fórmula Fn = Fn-1 + Fn-2. Utilize para 
+ * este exercício as variáveis ATUAL, ANTERIOR e 
+ * PRÓXIMO.
+ * JOSÉ AUGUSTO N. G. MANZANO, JAYR FIGUEIREDO DE 
+ * OLIVEIRA. Algoritmos (Portuguese Edition) (p. 285). 
+ * Editora Saraiva. Edição do Kindle. 
+ * Pedro, 15/09/2024
+ */
+import java.util.Scanner;
+
+public class Fibonacci09 {
+   public static void main(String[] args) {
+
+      // objeto
+      Scanner input = new Scanner(System.in);
+
+      // variáveis
+      int anterior = 0;
+      int atual = 1;
+      int proximo;
+
+      // entrada de dados
+      System.out.print("Digite o número inteiro: ");
+      proximo = input.nextInt();
+
+      System.out.print(anterior + " ");
+
+      // loop para contar
+      for(int i = 1; i < proximo; i++) {
+
+         // se for o primeiro valor de i
+         if (i == 1) {
+            // atual recebe 1
+            atual = 1;
+            // e o anterior recebe zero
+            anterior = 0;
+         } else { // se não
+            // o atual soma o valor anterior
+            atual += anterior;
+            // e o anterior recebe o valor atual
+            // diminuido do anterior
+            anterior = atual - anterior;
+         } // end if
+         // e exibe o valor atual
+         System.out.print(atual + " ");
+      } // end for
+
+      input.close();
+      
+   } // end main
+} // end class
