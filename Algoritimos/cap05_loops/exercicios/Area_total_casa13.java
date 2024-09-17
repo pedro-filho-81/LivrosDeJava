40505f4fc98fbf4d071de27c3b77e6c3f2571469
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-17 11:08:01 -0300

    Create Area_total_casa13.java

---
+++ Algoritimos/cap05_loops/exercicios/Area_total_casa13.java
@@ -0,0 +1,20 @@
+package cap05_loops.exercicios;
+/**
+ * 
+ * Pedro, 17/09/2024
+ */
+import java.util.Scanner;
+
+public class Area_total_casa13 {
+   public static void main(String[] args) {
+      
+      // variáveis
+      String nomeDoComodo = "";
+      double larg01 = 1, larg02 = 1;
+
+      // entrada de dados
+      System.out.println("Área de uma casa (apartamento, etc): ");
+      System.out.println("No");
+
+   } // end main 
+} // end class
