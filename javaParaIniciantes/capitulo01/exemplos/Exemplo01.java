98849c7e977d2c45299f971a2451d658acdbaa51
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-25 17:12:09 -0300

    Create Exemplo01.java

---
+++ javaParaIniciantes/capitulo01/exemplos/Exemplo01.java
@@ -0,0 +1,12 @@
+package javaParaIniciantes.capitulo01.exemplos;
+/**
+ * primeiro programa olá mundo!
+ */
+// cria a classe Exemplo01
+public class Exemplo01 {
+   // um programa java começa com o método main
+   public static void main(String[] args) {
+      // exibe na tela
+      System.out.println("Java impulsiona a web");
+   } // end main
+} // end class
