3cb1c9fe2ce48ec3c06435f50cf6cf4ac6116060
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-25 17:12:15 -0300

    Create Exemplo02.java

---
+++ javaParaIniciantes/capitulo01/exemplos/Exemplo02.java
@@ -0,0 +1,24 @@
+package javaParaIniciantes.capitulo01.exemplos;
+/**
+ * Demonstra uma variável
+ */
+public class Exemplo02 {
+   public static void main(String[] args) {
+
+      // variables
+      int var1; // declare a variable
+      int var2; // declare another variable
+
+      // var01 receives 1024
+      var1 = 1024;
+
+      // displar result
+      System.out.println("var01 contains: " + var1);
+
+      // var02 receives var01 / 2
+      var2 = var1 / 2;
+
+      // display result
+      System.out.println("var2 contains var1 / 2: " + var2);
+   } // end main
+} // end class
