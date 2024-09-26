f3a14081bbea20c065070cc50043de4821b958dc
PedroSantosFilho <psfilho81@hotmail.com>
2024-09-26 10:34:02 -0300

    Create ForDemo.java

---
+++ javaParaIniciantes/capitulo01/exemplos/instrucaoFOR/ForDemo.java
@@ -0,0 +1,17 @@
+package javaParaIniciantes.capitulo01.exemplos.instrucaoFOR;
+/**
+ * Exemplo do laço for
+ */
+public class ForDemo {
+   public static void main(String[] args) {
+      
+      // declare variable
+      int count; 
+
+      // loop for to display the values fo 1 to 5
+      for(count = 0; count < 5; count++) {
+         // display
+         System.out.println("This is count: " + count);
+      } // end for
+   } // end main
+} // end class
